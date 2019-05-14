104ba30039daea190b5434954634401fc2c6acf0
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-14 13:13:02 -0600

    task0001 - two sum: gulp task added

--- task0001-two-sum/twosum.js
+++ task0001-two-sum/twosum.js
@@ -4,7 +4,8 @@
  * @param {number}    target    the target sum to search
  * @return {number[]}           the indexes of the two numbers
  */
-const twoSum = function(nums, target) {
+const twoSum1 = function(nums, target) {
+  // brutal force to check every combination
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -25,9 +26,11 @@ const twoSum = function(nums, target) {
  */
 const twoSum2 = function(nums, target) {
   const obj = {};
+  // first create a hash map
   for (let i = 0; i < nums.length; i++) {
     obj[`${nums[i]}`] = i;
   }
+  // then search target using the hash map
   for (let i = 0; i < nums.length; i++) {
     const num = target - nums[i];
     if (obj[`${num}`] >= 0) {
@@ -45,17 +48,36 @@ const twoSum2 = function(nums, target) {
  * @param {number}    target    the target sum to search
  * @return {number[]}           the indexes of the two numbers
  */
-const twoSum3 = function(nums, target) {
+const twoSum = function(nums, target) {
   const obj = {};
+  // create a hash map
   for (let i = 0; i < nums.length; i++) {
     const num = target - nums[i];
+    // search target at the same time: return if found
     if (obj[`${num}`] >= 0) {
       return [i, obj[`${num}`]];
     }
+    // keep creating the hash map if not found
     obj[`${nums[i]}`] = i;
   }
   return [];
 };
 
 
-module.exports = twoSum3;
+/**
+ * main function
+ * @param {function}  callback    the callback function
+ */
+const main = (callback) => {
+  console.log('Task 0001 - Two Sum:');
+  const nums = [2, 7, 11, 15];
+  const target = 9;
+  console.log(`  target=${target}, nums=[${nums}]`);
+  console.log(`  result=[${twoSum(nums, target)}]`);
+  if (callback) {
+    callback();
+  }
+};
+
+
+module.exports = main;
