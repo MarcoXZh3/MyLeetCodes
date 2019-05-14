a4158075992dad668a774d4b4a1f9e6eda92f598
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-14 13:13:40 -0600

    task0002 - add two nums: gulp task added

--- task0002-add-two-num/add-two-num.js
+++ task0002-add-two-num/add-two-num.js
@@ -10,6 +10,33 @@ function ListNode(val, next) {
 
 
 /**
+ * Add two nums:
+ * @param {ListNode}  l1      the first list
+ * @param {ListNode}  l2      the second list
+ * @return {ListNode}         the sum of the two lists
+ */
+const addTwoNumbers = (l1, l2) => {
+  const re = new ListNode(0);
+  let cur = re;
+  // repeat whenever there is at least 1 number
+  while (l1 || l2) {
+    let num1 = l1 ? l1.val : 0;   // if current number not available, set 0
+    let num2 = l2 ? l2.val : 0;
+    l1 = l1 ? l1.next : null;     // move next, null if already not available
+    l2 = l2 ? l2.next : null;
+    const sum = cur.val + num1 + num2;  // summation
+    cur.val = sum % 10;                 // remainder as the current value
+    // move next only if carry or at least 1 next available
+    if (l1 || l2 || sum > 9) {
+      cur.next = new ListNode(sum > 9 ? 1 : 0); // set carry as the init value
+      cur = cur.next;
+    }
+  }
+  return re;
+};
+
+
+/**
  * Print the singly-linked list
  * @param {ListNode}  head    first node of the list
  */
@@ -24,38 +51,13 @@ const printList = (head) => {
 
 
 /**
- * Add two nums:
- * @param {ListNode}  l1      the first list
- * @param {ListNode}  l2      the second list
- * @return {ListNode}         the sum of the two lists
- */
-const addTwoNumbers = (l1, l2) => {
-  const re = new ListNode(0);
-  let cur = re;
-  while (l1 || l2) {
-    let num1 = l1 ? l1.val : 0;
-    let num2 = l2 ? l2.val : 0;
-    l1 = l1 ? l1.next : null;
-    l2 = l2 ? l2.next : null;
-    const sum = cur.val + num1 + num2;
-    cur.val = sum % 10;
-    if (l1 || l2 || sum > 9) {
-      cur.next = new ListNode(sum > 9 ? 1 : 0);
-      cur = cur.next;
-    }
-  }
-  return re;
-};
-
-
-/**
  * main function
  * @param {function}  callback    the callback function
  */
 const main = (callback) => {
+  console.log('Task 0002 - Add Two Numbers:');
   const num1 = [2, 4, 3];
   const num2 = [5, 6, 8, 2];
-
   const l1 = new ListNode(num1[0]);
   let ln1 = l1;
   for (let i = 1; i < num1.length; i++) {
@@ -69,9 +71,9 @@ const main = (callback) => {
     ln2 = ln2.next;
   }
 
-  console.log(printList(l1));
-  console.log(printList(l2));
-  console.log(printList(addTwoNumbers(l1, l2)));
+  console.log(`  ${printList(l1)}`);
+  console.log(`+ ${printList(l2)}`);
+  console.log(`= ${printList(addTwoNumbers(l1, l2))}`);
   if (callback) {
     callback();
   }
