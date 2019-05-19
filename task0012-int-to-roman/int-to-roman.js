9bb193adcd69a9d0f587a912f12de197d28e1e2c
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-18 21:05:50 -0600

    task0012 - integer to roman: finished

--- task0012-int-to-roman/int-to-roman.js
+++ task0012-int-to-roman/int-to-roman.js
@@ -3,7 +3,34 @@
  * @return {string}           the roman number
  */
 const intToRoman = function(num) {
+  const chars = [
+    ['I', 'V'],     // 1 and 5
+    ['X', 'L'],     // 10 and 50
+    ['C', 'D'],     // 100 and 500
+    ['M'],          // 1000
+  ];
 
+  let roman = '';
+  let cnt = 0;      // how many digits processed
+  while (num > 0) {
+    let roman1 = '';
+    const d = num % 10;           // the digit
+    const c = Math.floor(d / 5);  // carry by dividing 5
+    let r = d % 5;                // remainder by dividing 5
+    if (r === 4) {                                    // case 1: "4"
+      roman1 = chars[cnt][0] + chars[cnt + c][1 - c]; // prepend "1" to next char
+    } else {                                          // case 2: other numbers
+      roman1 = c === 1 ? chars[cnt][c] : '';          // starts with current char
+      while (r -- > 0) {
+        roman1 += chars[cnt][0];                      // append extra "1"s
+      }
+    }
+    // move to next digit
+    roman = roman1 + roman;
+    cnt ++;
+    num = Math.floor(num / 10);
+  }
+  return roman;
 };
 
 
