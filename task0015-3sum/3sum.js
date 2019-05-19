9a482bfddf2452ac588ff0511b77fe3dcc6ff68d
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-19 15:17:43 -0600

    task0015 - 3sum: finished

--- task0015-3sum/3sum.js
+++ task0015-3sum/3sum.js
@@ -3,7 +3,34 @@
  * @return {number[][]}             all possible solutions
  */
 const threeSum = function(nums) {
-  return [];
+  const res = [];
+  const sum3 = 0;
+  nums = nums.sort( (a,b)=>a-b );
+  // loop all num1s
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {   // skip same num1s
+      continue;
+    }
+    const num1 = nums[i];
+    const sum2 = sum3 - num1;
+    let j = i + 1;
+    let k = nums.length - 1;
+    // approaching num2 and num3 to sum2
+    while (j < k) {
+      const num2 = nums[j];
+      const num3 = nums[k];
+      if (num2 + num3 < sum2) {
+        j++;
+      } else if (num2 + num3 > sum2) {
+        k--;
+      } else {
+        res.push([num1, num2, num3]);
+        while (j < k && nums[++j] === num2) ; // skip same num2s
+        while (j < k && nums[--k] === num3) ; // skip same num3s
+      }
+    }
+  }
+  return res;
 };
 
 
@@ -15,6 +42,8 @@ const main = (callback) => {
   console.log('Task 0015 - 3 Sum:');
   [
     [-1, 0, 1, 2, -1, -4],
+    [0, 0, 0],
+    [-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6],
   ].forEach( vs => {
     const res = threeSum(vs);
     console.log(`  source=[${vs.join(', ')}]`);
