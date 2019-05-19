4fd4bb1bd3233289046a69d4633d2c5c3f58c701
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-18 21:26:07 -0600

    task0013 - roman to integer: finished

--- task0013-roman-to-int/roman-to-int.js
+++ task0013-roman-to-int/roman-to-int.js
@@ -3,7 +3,30 @@
  * @return {number}         the integer
  */
 const romanToInt = function(s) {
+  const values = {
+    'I':  1,
+    'V':  5,
+    'X':  10,
+    'L':  50,
+    'C':  100,
+    'D':  500,
+    'M':  1000,
+  };
 
+  let num = 0;
+  while (s.length > 0) {
+    let char = s.charAt(0);
+    if (s.length > 1 &&     // case 1: "4" or "9" - two chars as 1 value
+        ['I', 'X', 'C'].includes(char) &&
+        values[s.charAt(1)] > values[char]) {
+      num += values[s.charAt(1)] - values[char];
+      s = s.substr(2);
+    } else {                // case 2: others - 1 char as 1 value
+      num += values[char];
+      s = s.substr(1);
+    }
+  }
+  return num;
 };
 
 
