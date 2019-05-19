c1d1dd769289eab85c17df92c7f6e612bb1e889f
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-18 20:09:22 -0600

    task0011-most-water: finished

--- task0011-most-water/most-water.js
+++ task0011-most-water/most-water.js
@@ -1,9 +1,45 @@
 /**
+ * brutal force with two-level iteration
+ * @param {number[]}    height    the list of heights
+ * @return {number}               the max area
+ */
+const maxArea1 = function(height) {
+  let max = 0;
+  for(let i = 0; i < height.length - 1; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const area = Math.min(height[i], height[j]) * (j - i);
+      if (max < area) {
+        max = area;
+      }
+    }
+  }
+  return max;
+};
+
+
+/**
+ * two pointer: keep larger height; look for a better shorter height
  * @param {number[]}    height    the list of heights
  * @return {number}               the max area
  */
 const maxArea = function(height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+    if (max < area) {
+      max = area;
+    }
 
+    // the next possible solution will never use the shorter of current pair
+    if (height[left] > height[right]) {
+      right --;
+    } else {
+      left ++;
+    }
+  }
+  return max;
 };
 
 
