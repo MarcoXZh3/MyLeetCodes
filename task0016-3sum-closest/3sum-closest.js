9a8ae31cb455184bf2113eefc561e8eaf3c2f32a
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-19 19:32:01 -0600

    task0016 - 3sum closest: finished

--- task0016-3sum-closest/3sum-closest.js
+++ task0016-3sum-closest/3sum-closest.js
@@ -4,7 +4,24 @@
  * @return {number}               the closest sum
  */
 const threeSumClosest = function(nums, target) {
-
+  let re = nums[0] + nums[1] + nums[nums.length - 1];
+  nums = nums.sort( (a,b)=>a-b );
+  for (let i = 0; i < nums.length - 2; i++) {
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum > target) {
+        k --;
+      } else {
+        j ++;
+      }
+      if (Math.abs(sum - target) < Math.abs(re - target)) {
+        re = sum;
+      }
+    }
+  }
+  return re;
 };
 
 
