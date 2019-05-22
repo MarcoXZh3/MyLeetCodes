ad2254027f8cd6b887004bb32e42f35aea3f3d4f
MarcoXZh3 <marcoxzh3@gmail.com>
2019-05-22 11:16:24 -0600

    task0027 - remove element: finished

--- task0027-del-elm/del-elm.js
+++ task0027-del-elm/del-elm.js
@@ -1,10 +1,40 @@
 /**
+ * identify non-val elements and keep them
+ * @param {number[]}    nums    the sorted array of numbers
+ * @param {number}      val     the value to be removed
+ * @return {number}             the length of the non-duplicated array
+ */
+const removeElement1 = function(nums, val) {
+  // tail: the first element that is not val
+  let tail = -1;
+  while (++tail < nums.length && nums[tail] !== val) ;
+
+  // starting from tail, move elements that are not val to tail
+  for (let i = tail; i < nums.length; i++) {
+    // elements equal to neither val nor tail need to be preserved
+    if (nums[i] !== val && i !== tail) {
+      nums[tail++] = nums[i];
+    }
+  }
+  return tail;
+};
+
+
+/**
+ * identify val elements and delete them
  * @param {number[]}    nums    the sorted array of numbers
  * @param {number}      val     the value to be removed
  * @return {number}             the length of the non-duplicated array
  */
 const removeElement = function(nums, val) {
-
+  let target = -1;
+  while (++target < nums.length && nums[target] !== val) ;
+  for (let i = target + 1; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[target++] = nums[i];
+    }
+  };
+  return target;
 };
 
 
@@ -18,9 +48,9 @@ const main = (callback) => {
     [ [3, 2, 2, 3], 3],
     [ [0, 1, 2, 2, 3, 0, 4, 2], 2],
   ].forEach( vs => {
-    console.log(`  list=[${vs[0]}]`);
+    console.log(`  val=${vs[1]}, list=[${vs[0]}]`);
     const length = removeElement(vs[0], vs[1]);
-    console.log(`  new=[${vs[0].slice(0, length)}], n=${vs[1]}`);
+    console.log(`  n=${length}, new=[${vs[0].slice(0, length)}]`);
   });
   if (callback) {
     callback();
