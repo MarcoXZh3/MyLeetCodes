b65e009430ae511a327c7712dea851813f219ff5
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-06-26 09:17:40 -0600

    task0035 - search insert position: finished

--- task0035-isrt-pos/insert-pos.js
+++ task0035-isrt-pos/insert-pos.js
@@ -1,10 +1,45 @@
 /**
+ * linear search
+ * @param {number[]}    nums      the sorted array
+ * @param {number}      target    the value to search
+ * @return {number}               element position or insert position
+ */
+const searchInsert1 = function(nums, target) {
+  let idx = 0;
+  for (; idx < nums.length; idx++) {
+    if (nums[idx] >= target) {
+      break ;
+    }
+  }
+  return idx;
+};
+
+
+/**
+ * binary search
  * @param {number[]}    nums      the sorted array
  * @param {number}      target    the value to search
  * @return {number}               element position or insert position
  */
 const searchInsert = function(nums, target) {
-
+  let l = 0;
+  let r = nums.length - 1;
+  let m;
+  let greater = 0;    // whether target is greater than the median
+  while (l <= r) {
+    m = Math.floor((l + r) / 2);
+    if (target < nums[m]) {
+      r = m - 1;
+      greater = 0;
+    } else if (target > nums[m]) {
+      l = m + 1;
+      greater = 1;
+    } else {
+      return m;
+    }
+  }
+  // the target position will be the median or its next when target is greater
+  return m + greater;
 };
 
 
