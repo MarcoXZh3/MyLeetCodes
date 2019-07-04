161774c1d8844641189032f444dc0313c4c38868
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-07-04 10:06:08 -0600

    task0040 - combine sum 2: finished

--- task0040-comb-sum-2/comb-sum-2.js
+++ task0040-comb-sum-2/comb-sum-2.js
@@ -1,10 +1,48 @@
 /**
+ * recursively calculate the combined sum, return null or the solutions
+ * same with previous question except skipping duplicated candidates
+ * @param {number[]}      candidates    the current value list
+ * @param {number}        target        the target sum
+ * @return {number[][]}                 all its possible solutions
+ */
+const getCombSum2 = function(candidates, target) {
+  // terminate when target becomes 0, or no possible solutions available
+  if (target === 0) {
+    return [[]];      // target becomes 0: return empty sub-solutions
+  } else if (candidates.length === 0 || candidates[0] > target) {
+    return null;      // no possible solutions
+  }
+
+  // collect the current solutions based on upcoming sub-solutions
+  const res = [];
+  for (let i = 0; i < candidates.length; i++) {
+    // skip duplicated candidates
+    if (i > 0 && candidates[i - 1] === candidates[i]) {
+      continue ;
+    }
+    // don't consider itself and smaller numbers for next recursion
+    const res0 = getCombSum2(candidates.slice(i+1), target - candidates[i]);
+    if (!res0) {
+      continue ;
+    }
+    // when sub-solutions available, combine current value as current solutions
+    for (let re of res0) {
+      re.unshift(candidates[i]);
+      res.push(re);
+    }
+  }
+  return res.length === 0 ? null : res;
+};
+
+
+/**
  * @param {number[]}      candidates    the value list
  * @param {number}        target        the target sum
  * @return {number[][]}                 all possible solutions
  */
 const combinationSum2 = function(candidates, target) {
-  return [[]];
+  // the recursion assumes the candidates are sorted
+  return getCombSum2(candidates.sort( (a,b)=>a-b ), target) || [];
 };
 
 
