db7132e5773d35f163666cb2d7c532c050cc266a
marcoxzh3 <marcox@durabuiltwindows.com>
2019-07-19 12:05:44 -0600

    task0053 - max subarray: finished

--- task0053-max-subarray/max-subarray.js
+++ task0053-max-subarray/max-subarray.js
@@ -1,9 +1,37 @@
 /**
+ * brutal force: check every possible sum
+ * @param {number[]}  nums    the number array
+ * @return {number}           sum of the max sub-array
+ */
+const maxSubArray0 = function(nums) {
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      if (max < sum) {
+        max = sum;
+      }
+    }
+  }
+  return max;
+};
+
+
+/**
  * @param {number[]}  nums    the number array
  * @return {number}           sum of the max sub-array
  */
 const maxSubArray = function(nums) {
-
+  const dp = [nums[0]];
+  let max = dp[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp.push(nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0));
+    if (max <dp[i]) {
+      max = dp[i];
+    }
+  }
+  return max;
 };
 
 
