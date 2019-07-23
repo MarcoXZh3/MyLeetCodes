5a79eb8582cc9964b938844ccd5af667e333cf6f
marcoxzh3 <marcox@durabuiltwindows.com>
2019-07-23 16:15:10 -0600

    task0059 - spiral matrix 2: finished

--- task0059-spiral-matrix-2/spiral-mat-2.js
+++ task0059-spiral-matrix-2/spiral-mat-2.js
@@ -3,7 +3,32 @@
  * @return {number[][]}         the spiral matrix
  */
 const generateMatrix = function(n) {
-  return [];
+  const res = [...Array(n).keys()].map( _=>[...Array(n).keys()].map( _=>0 ) );
+  const visited = [...Array(n).keys()].map( _=>[...Array(n).keys()].map( _=>false ) );
+  const xDir = [0, 1, 0, -1];
+  const yDir = [1, 0, -1, 0];
+  let dir = 0;
+  let x = 0;
+  let y = 0;
+  res[x][y] = 1;
+  visited[x][y] = true;
+  for (let i = 2; i <= n * n; i++) {
+    const xNext = x + xDir[dir];
+    const yNext = y + yDir[dir];
+    if (xNext >= 0 && xNext < n &&
+        yNext >= 0 && yNext < n &&
+        !visited[xNext][yNext]) {
+      x = xNext;
+      y = yNext;
+    } else {
+      dir = (dir + 1) % 4;
+      x = x + xDir[dir];
+      y = y + yDir[dir];
+    }
+    res[x][y] = i;
+    visited[x][y] = true;
+  }
+  return res;
 };
 
 
