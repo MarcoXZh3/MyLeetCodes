808058149f012068fbc75236404016ad3c23c8e5
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-02 16:30:20 -0600

    task0071 - simplify path: finished

--- task0071-simple-path/simple-path.js
+++ task0071-simple-path/simple-path.js
@@ -3,7 +3,21 @@
  * @return {string}             the simplified path
  */
 const simplifyPath = function(path) {
-
+  const paths = path.split('/');
+  const stack = [];
+  for (let i = 0; i < paths.length; i++) {
+    const folder = paths[i];
+    if (!folder || folder === '.') {
+      continue ;
+    } else if (folder === '..') {
+      if (stack.length > 0) {
+        stack.pop();
+      }
+    } else {
+      stack.push(folder);
+    }
+  }
+  return `/${stack.join('/')}`;
 };
 
 
