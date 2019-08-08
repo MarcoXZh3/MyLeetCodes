6279b389a23ceab8173118e31a46c36d86aaf927
MarcoXZh3 <marcoxzh3@gmail.com>
2019-08-07 20:13:59 -0600

    task0075 - sort colors: finished

--- task0075-sort-colors/sort-colors.js
+++ task0075-sort-colors/sort-colors.js
@@ -1,9 +1,85 @@
 /**
+ * bubble sort
+ * @param {number[]}    nums    the number list representing colors
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors0 = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] < nums[j]) {
+        const tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+      }
+    }
+  }
+};
+
+
+/**
+ * O(2n)
+ * @param {number[]}    nums    the number list representing colors
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors1 = function(nums) {
+  const cnt = [0, 0, 0];
+  for (let i  = 0; i < nums.length; i++) {
+    cnt[nums[i]] ++;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (i < cnt[0]) {
+      nums[i] = 0;
+    } else if (i >= nums.length - cnt[2]) {
+      nums[i] = 2;
+    } else {
+      nums[i] = 1;
+    }
+  }
+};
+
+
+/**
+ * O(n)
+ * @param {number[]}    nums    the number list representing colors
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors2 = function(nums) {
+  let zero = 0;
+  let two = nums.length - 1;
+  for (let i = 0; i <= two; i++) {
+    while (nums[i] === 2 && i < two) {
+      const tmp = nums[i];
+      nums[i] = nums[two];
+      nums[two--] = tmp;
+    }
+    while (nums[i] === 0 && i > zero) {
+      const tmp = nums[i];
+      nums[i] = nums[zero];
+      nums[zero++] = tmp;
+    }
+  }
+};
+
+
+/**
+ * O(n)
  * @param {number[]}    nums    the number list representing colors
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const sortColors = function(nums) {
-
+  let zero = 0;
+  let two = nums.length - 1;
+  for (let i = 0; i <= two; i++) {
+    if (nums[i] === 0 && i !== zero) {
+      const tmp = nums[i];
+      nums[i--] = nums[zero];
+      nums[zero++] = tmp;
+    } else if (nums[i] === 2 && i !== two) {
+      const tmp = nums[i];
+      nums[i--] = nums[two];
+      nums[two--] = tmp;
+    }
+  }
 };
 
 
