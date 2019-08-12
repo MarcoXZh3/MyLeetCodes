bdbefbd97382f2806f5e5e70fd9e0a550faa6c3e
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-12 17:02:44 -0600

    task0078 - subsets: finished

--- task0078-subsets/subsets.js
+++ task0078-subsets/subsets.js
@@ -1,9 +1,52 @@
 /**
+ * @param {number[][]}    res       all the results
+ * @param {number[]}      cur       current result
+ * @param {number[][]}    nums      all numbers to grab
+ * @param {number[]}      length    how many numbers to grab
+ */
+const backtracking = function(res, cur, nums, length) {
+  if (length === 0) {
+    res.push(JSON.parse(JSON.stringify(cur)));
+    return ;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    cur.push(nums[i]);
+    backtracking(res, cur, nums.filter( n=>n>nums[i] ), length - 1);
+    cur.pop();
+  }
+};
+
+
+/**
+ * backtracking
+ * @param {number[]}      nums    numbers to grab for the subsets
+ * @return {number[][]}           all subsets
+ */
+const subsets1 = function(nums) {
+  nums = nums.sort( (a,b)=>a-b );
+  const res = [];
+  for (let i = 0; i <= nums.length; i++) {
+    backtracking(res, [], nums, i);
+  }
+  return res;
+};
+
+
+/**
+ * copy the previous results and each adding a number as the current results
  * @param {number[]}      nums    numbers to grab for the subsets
  * @return {number[][]}           all subsets
  */
 const subsets = function(nums) {
-  return [nums];
+  const res = [[]];
+  for (let num of nums) {
+    const length = res.length;
+    for (let i = 0; i < length; i++) {
+      res.push(JSON.parse(JSON.stringify(res[i])));
+      res[res.length - 1].push(num);
+    }
+  }
+  return res;
 };
 
 
