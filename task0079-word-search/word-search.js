7ca2447a862c744a0fd79c06ea2f3ed8b62ccc34
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-12 17:30:52 -0600

    task0079 - word search: finished

--- task0079-word-search/word-search.js
+++ task0079-word-search/word-search.js
@@ -1,10 +1,46 @@
 /**
+ * @param {character[][]} board   the board of letters
+ * @param {number}        i       row index to check the current char
+ * @param {number}        j       column index to check the current char
+ * @param {string}        word    the current word to check
+ */
+const backtracking = function(board, i, j, word) {
+  // check the first char
+  if (board[i][j] !== word.charAt(0)) {
+    return false;
+  }
+  // mark the current char as visited
+  board[i][j] += '!';
+  // check the next chars recursively
+  const sub = word.substring(1);
+  // no more chars, or all next chars matched - fully matched
+  if (sub.length === 0 ||
+      i - 1 >= 0 && backtracking(board, i - 1, j, sub) ||
+      i + 1 < board.length && backtracking(board, i + 1, j, sub) ||
+      j - 1 >= 0 && backtracking(board, i, j - 1, sub) ||
+      j + 1 < board[0].length && backtracking(board, i, j + 1, sub)) {
+    return true;
+  }
+  // otherwise restore the current char and return not matched
+  board[i][j] = board[i][j].charAt(0);
+  return false;
+};
+
+
+/**
  * @param {character[][]}   board   the board of letters
  * @param {string}          word    the word to search
  * @return {boolean}                whether the word is in the board or not
  */
 const exist = function(board, word) {
-
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (backtracking(board, i, j, word)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
