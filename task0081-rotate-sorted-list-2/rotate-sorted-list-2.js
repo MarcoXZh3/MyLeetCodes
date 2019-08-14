71e89525a4195f4d4613fa2e2ad54c450709f9dd
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-14 12:06:44 -0600

    task0081 - rotated sorted array II: finished

--- task0081-rotate-sorted-list-2/rotate-sorted-list-2.js
+++ task0081-rotate-sorted-list-2/rotate-sorted-list-2.js
@@ -4,9 +4,36 @@
  * @return {boolean}            whether found or not
  */
 const search = function(nums, target) {
-
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return true;
+    }
+    if (nums[left] < nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else if (nums[mid] < nums[right]) {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    } else {
+      if (nums[left] === nums[mid]) {
+        left ++;
+      }
+      if (nums[right] === nums[mid]) {
+        right --;
+      }
+    }
+  }
+  return nums[left] === target;
 };
-
 
 /**
  * main function
@@ -17,6 +44,7 @@ const main = (callback) => {
   [
     [[2, 5, 6, 0, 0, 1, 2], 0],
     [[2, 5, 6, 0, 0, 1, 2], 3],
+    [[1, 1, 1, 1, 1, 1, 2, 1, 1, 1], 2],  // solution of Task 33 won't work
   ].forEach( vs => {
     console.log(`  found=${search(vs[0], vs[1])}, target=${vs[1]}, nums=[${vs[0].join(', ')}]`);
   });
