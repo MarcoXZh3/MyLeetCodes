7d67d9886395a4f3350ae8b7c7ef535aad2fd113
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-19 15:27:41 -0600

    task0085 - max rect in matrix: finished

--- task0085-max-mat-rect/max-mat-rect.js
+++ task0085-max-mat-rect/max-mat-rect.js
@@ -3,7 +3,43 @@
  * @return {number}                   the area of the max rectangle
  */
 const maximalRectangle = function(matrix) {
-
+  if (!matrix || matrix.length === 0) {
+    return 0;
+  }
+  let m = matrix.length;
+  let n = matrix[0].length;
+  const left = Array(n).fill(0);
+  const right = Array(n).fill(n);
+  const height = Array(n).fill(0);
+  let max = 0;
+  for (let i = 0; i < m; i++) {
+    let l = 0;
+    let r = n;
+    for (let j = 0; j < n; j++) {
+      height[j] = matrix[i][j] === '1' ? height[j] + 1 : 0;
+    }
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === '1') {
+        left[j] = left[j] > l ? left[j] : l;
+      } else {
+        left[j] = 0;
+        l = j + 1;
+      }
+    }
+    for (let j = n - 1; j >= 0; j--) {
+      if (matrix[i][j] === '1') {
+        right[j] = right[j] < r ? right[j] : r;
+      } else {
+        right[j] = n;
+        r = j;
+      }
+    }
+    for (let j = 0; j < n; j++) {
+      const area = (right[j] - left[j]) * height[j];
+      max = max > area ? max : area;
+    }
+  }
+  return max;
 };
 
 
