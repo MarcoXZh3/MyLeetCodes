ed56b086fab5cbcd6978ed032a623f80c7e44b1c
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-19 16:04:20 -0600

    task0086 - partition list: finished

--- task0086-partition-list/partition-list.js
+++ task0086-partition-list/partition-list.js
@@ -15,7 +15,23 @@ function ListNode(val, next) {
  * @return {ListNode}         the head of the list after partition
  */
 const partition = function(head, x) {
-  return head;
+  const dummy1 = new ListNode(null);
+  const dummy2 = new ListNode(null);
+  let cur1 = dummy1;
+  let cur2 = dummy2;
+  while (head) {
+    if (head.val < x) {
+      cur1.next = head;
+      cur1 = cur1.next;
+    } else {
+      cur2.next = head;
+      cur2 = cur2.next;
+    }
+    head = head.next;
+  }
+  cur1.next = dummy2.next;
+  cur2.next = null;
+  return dummy1.next;
 };
 
 
