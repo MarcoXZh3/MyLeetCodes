5e1db6d79a931de54c4e1410dae87a0a93be9ed7
MarcoXZh3 <marcoxzh3@gmail.com>
2019-08-20 19:53:43 -0600

    task0090 - subset 2: finished

--- task0090-subsets-2/subsets-2.js
+++ task0090-subsets-2/subsets-2.js
@@ -1,9 +1,63 @@
 /**
- * @param {number[]}      nums      the number array
- * @return {number[][]}             all the sub-sets
+ * @param {number[][]}    res       all the results
+ * @param {number[]}      cur       current result
+ * @param {number[][]}    nums      all numbers to grab
+ * @param {number[]}      length    how many numbers to grab
+ */
+const backtracking = function(res, cur, nums, length, map) {
+  if (length === 0) {
+    const key = cur.sort( (a,b)=>a-b ).join('');
+    if (!map[key]) {
+      res.push(JSON.parse(JSON.stringify(cur)));
+      map[key] = 1;
+    }
+    return ;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    cur.push(nums[i]);
+    backtracking(res, cur, nums.filter( (_,j)=>j>i ), length - 1, map);
+    cur.pop();
+  }
+};
+
+
+/**
+ * backtracking
+ * @param {number[]}      nums    numbers to grab for the subsets
+ * @return {number[][]}           all subsets
+ */
+const subsetsWithDup0 = function(nums) {
+  nums = nums.sort( (a,b)=>a-b );
+  const res = [];
+  for (let i = 0; i <= nums.length; i++) {
+    backtracking(res, [], nums, i, {});
+  }
+  return res;
+};
+
+
+/**
+ * copy the previous results and each adding a number as the current results
+ * @param {number[]}      nums    numbers to grab for the subsets
+ * @return {number[][]}           all subsets
  */
 const subsetsWithDup = function(nums) {
-  return [];
+  const res = [[]];
+  const map = { '':1 };
+  for (let num of nums) {
+    const length = res.length;
+    for (let i = 0; i < length; i++) {
+      const cur = JSON.parse(JSON.stringify(res[i]));
+      cur.push(num);
+      const key = cur.sort( (a,b)=>a-b ).join('');
+      if (map[key]) {
+        continue ;
+      }
+      res.push(cur);
+      map[key] = 1;
+    }
+  }
+  return res;
 };
 
 
