3455c0f065692da3daaadc94b81d9eddf8c80cf7
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-22 15:47:07 -0600

    data structure update: binary tree node and array to binary tree

--- _utils/tree.js
+++ _utils/tree.js
@@ -31,28 +31,65 @@ const TreeNode = module.exports.TreeNode = function(val, p, cs) {
  */
 const BinaryTreeNode = module.exports.BinaryTreeNode = function(val, p, l, r) {
   /**
-   * parse left and right child
+   * value of the node
    */
-  let children = [];
-  if (l && l.constructor.name === [].constructor.name) {
-    children = l;
-  } else if (l && l.constructor === this.constructor) {
-    children = [l, r];
-  }
-
+  this.val = val;
 
   /**
-   * inherits basic tree node
+   * parent of the node
    */
-  TreeNode.call(this, val, p, children);
+  this.parent = p;
 
   /**
    * left child
    */
-  this.left = children[0];
+  this.left = l;
 
   /**
    * right child
    */
-  this.right = children[1];
+  this.right = r;
+};
+
+
+/**
+ * create a tree out of an array representing a complete binary tree
+ * @param {object[]}          vals      the number array
+ * @returns {BinaryTreeNode}            the root of the tree
+ */
+module.exports.array2bintree = function(vals) {
+  if (!vals || vals.length === 0) {
+    return null;
+  }
+
+  const root = new BinaryTreeNode(vals[0]);
+  const nodes = [root];   // the node array mapping to the number array vals
+  for (let i = 1; i < vals.length; i++) {
+    if (vals[i] === null) {
+      nodes.push(null);
+      continue ;
+    }
+    const p = Math.floor((i - 1) / 2);
+    const node = new BinaryTreeNode(vals[i]);
+    if (p * 2 + 1 === i) {
+      nodes[p].left = node;
+    } else {
+      nodes[p].right = node;
+    }
+    nodes.push(node);
+  }
+  return root;
+};
+
+
+/**
+ * Print the tree in console
+ * @param {TreeNode}  root    root of the tree
+ * @returns {string}          the string representation of the tree
+ */
+module.exports.printTree = function(root) {
+  if (!root) {
+    return '{NULL}';
+  }
+  return '';
 };
