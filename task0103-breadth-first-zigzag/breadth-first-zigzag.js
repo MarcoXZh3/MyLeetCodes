96cbc42f57741e2c150b4c40f6721df781538d9d
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-28 17:47:31 -0600

    task0103 - binary tree breadth first zigzag: finished

--- task0103-breadth-first-zigzag/breadth-first-zigzag.js
+++ task0103-breadth-first-zigzag/breadth-first-zigzag.js
@@ -8,8 +8,60 @@ const TreeNode = BinaryTreeNode;
  * @param {TreeNode}      root    root of the tree
  * @return {number[][]}           tree node values by level
  */
+const zigzagLevelOrder0 = function(root) {
+  const recursion = (root, level, res) => {
+    if (!root) {
+      return ;
+    }
+    if (level >= res.length) {
+      res.push([]);
+    }
+    if (level % 2 === 1) {
+      res[level].unshift(root.val);
+    } else {
+      res[level].push(root.val);
+    }
+    recursion(root.left, level + 1, res);
+    recursion(root.right, level + 1, res);
+  };
+  const res = [];
+  recursion(root, 0, res);
+  return res;
+};
+
+
+/**
+ * iteration with queue
+ * @param {TreeNode}      root    root of the tree
+ * @return {number[][]}           tree node values by level
+ */
 const zigzagLevelOrder = function(root) {
-  return [];
+  const res = [];
+  if (!root) {
+    return res;
+  }
+  const queue = [root];
+  let direction = false;
+  while (queue.length > 0) {
+    direction = !direction;
+    const level = queue.length;
+    const vals = [];
+    for (let i = 0; i < level; i++) {
+      if (queue[queue.length - 1].left) {
+        queue.unshift(queue[queue.length - 1].left);
+      }
+      if (queue[queue.length - 1].right) {
+        queue.unshift(queue[queue.length - 1].right);
+      }
+      if (direction) {
+        vals.push(queue.pop().val);
+      } else {
+        vals.unshift(queue.pop().val);
+      }
+    }
+    res.push(vals);
+  }
+  return res;
 };
 
 
@@ -21,6 +73,7 @@ const main = (callback) => {
   console.log('Task 0103 - Binary Tree Zigzag Level Order Traversal:');
   [
     [3, 9, 20, null, null, 15, 7],
+    [1, 2, 3, 4, null, null, 5],
   ].forEach( vs => {
     const root = array2bintree(vs);
     console.log(printTree(root));
