a883df7fb99392a682daf314c83a29588f54dc14
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-29 16:28:52 -0600

    task0107 - breadth 1st reverse: finished

--- task0107-breadth-first-reverse/breadth-first-reverse.js
+++ task0107-breadth-first-reverse/breadth-first-reverse.js
@@ -7,8 +7,49 @@ const TreeNode = BinaryTreeNode;
  * @param {TreeNode}      root    root of the tree
  * @return {number[][]}           the 
  */
+const levelOrderBottom0 = function(root) {
+  const recursion = (root, level, res) => {
+    if (!root) {
+      return ;
+    }
+    if (level >= res.length) {
+      res.unshift([]);
+    }
+    res[res.length - 1 - level].push(root.val);
+    recursion(root.left, level + 1, res);
+    recursion(root.right, level + 1, res);
+  };
+  const res = [];
+  recursion(root, 0, res);
+  return res;
+};
+
+
+/**
+ * @param {TreeNode}      root    root of the tree
+ * @return {number[][]}           the 
+ */
 const levelOrderBottom = function(root) {
-  return [];
+  const res = [];
+  if (!root) {
+    return res;
+  }
+  const queue = [root];
+  while (queue.length > 0) {
+    const level = queue.length;
+    const vals = [];
+    for (let i = 0; i < level; i++) {
+      if (queue[queue.length - 1].left) {
+        queue.unshift(queue[queue.length - 1].left);
+      }
+      if (queue[queue.length - 1].right) {
+        queue.unshift(queue[queue.length - 1].right);
+      }
+      vals.push(queue.pop().val);
+    }
+    res.unshift(vals);
+  }
+  return res;
 };
 
 
