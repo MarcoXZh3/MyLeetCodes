33cb481a10188b918e1722bc118ca64c0bc4082a
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-29 16:45:13 -0600

    task0108 - sorted array to BST: finished

--- task0108-sorted-arr-to-bst/sorted-arr-to-bst.js
+++ task0108-sorted-arr-to-bst/sorted-arr-to-bst.js
@@ -8,7 +8,21 @@ const TreeNode = BinaryTreeNode;
  * @return {TreeNode}           the binary search tree
  */
 const sortedArrayToBST = function(nums) {
-
+  const recursion = (nums, start, end) => {
+    const mid = Math.floor((start + end) / 2);
+    if (mid < 0 || mid >= nums.length) {
+      return null;
+    }
+    const root = new TreeNode(nums[mid]);
+    if (start < mid) {
+      root.left = recursion(nums, start, mid);
+    }
+    if (mid + 1 < end) {
+      root.right = recursion(nums, mid + 1, end);
+    }
+    return root;
+  };
+  return recursion(nums, 0, nums.length);
 };
 
 
@@ -20,6 +34,7 @@ const main = (callback) => {
   console.log('Task 0108 - Convert Sorted Array to Binary Search Tree:');
   [
     [-10, -3, 0, 5, 9],
+    [3, 5, 8],
   ].forEach( vs => {
     console.log(`[${vs.join(', ')}]`);
     const root = sortedArrayToBST(vs);
