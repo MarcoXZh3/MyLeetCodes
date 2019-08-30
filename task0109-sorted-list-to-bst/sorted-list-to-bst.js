15e489c141bc3b9c91c6a3a1964718b508103cd0
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-08-30 16:05:08 -0600

    task0109 - sorted list to BST: finished

--- task0109-sorted-list-to-bst/sorted-list-to-bst.js
+++ task0109-sorted-list-to-bst/sorted-list-to-bst.js
@@ -9,7 +9,29 @@ const TreeNode = BinaryTreeNode;
  * @return {TreeNode}           the binary tree
  */
 const sortedListToBST = function(head) {
-
+  const recursion = (head) => {
+    let mid = head;
+    let prev = null;
+    let tail = head;
+    while (tail && tail.next) {
+      prev = mid;
+      mid = mid.next;                   // mid skips 1 node every time
+      tail = tail.next.next;            // while tail skips 2
+    }
+    if (!mid) {
+      return null;
+    }
+    const root = new TreeNode(mid.val);
+    if (prev) {                         // split the list at mid
+      prev.next = null;
+    } else {
+      head = null;
+    }
+    root.left = recursion(head);        // left half goes to left sub tree
+    root.right = recursion(mid.next);   // right half goes to right sub tree
+    return root;
+  };
+  return recursion(head);
 };
 
 
@@ -24,7 +46,7 @@ const main = (callback) => {
   ].forEach( vs => {
     const head = array2list(vs);
     console.log(printList(head));
-    const root = sortedListToBST(vs);
+    const root = sortedListToBST(head);
     console.log(printTree(root));
   });
   if (callback) {
