cc386852a7cea3b2e3fdcc551944e95c90f23790
MarcoXZh3 <marcoxzh3@gmail.com>
2019-09-02 16:48:21 -0600

    task0014 - flatten tree to list: finished

--- task0114-flat-tree-to-list/flat-tree-to-list.js
+++ task0114-flat-tree-to-list/flat-tree-to-list.js
@@ -4,11 +4,50 @@ const TreeNode = BinaryTreeNode;
 
 
 /**
+ * recursion
+ * @param {TreeNode}  root    root of the binary tree
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+const flatten0 = function(root) {
+  const recursion = (root) => {
+    if (!root) {
+      return null;
+    }
+    let flat = recursion(root.left);
+    const right = root.right;
+    if (flat) {
+      root.left = null;
+      root.right = flat;
+      while (flat.right) {
+        flat = flat.right;
+      }
+      flat.right = right;
+    }
+    recursion(right);
+    return root;
+  };
+  recursion(root);
+};
+
+
+/**
+ * iteration
  * @param {TreeNode}  root    root of the binary tree
  * @return {void} Do not return anything, modify root in-place instead.
  */
 const flatten = function(root) {
-
+  while (root) {
+    if (root.left) {
+      let prev = root.left;
+      while (prev.right) {
+        prev = prev.right;
+      }
+      prev.right = root.right;
+      root.right = root.left;
+      root.left = null;
+    }
+    root = root.right;
+  }
 };
 
 
@@ -20,6 +59,8 @@ const main = (callback) => {
   console.log('Task 0109 - Convert Sorted List to Binary Search Tree:');
   [
     [1, 2, 5, 3, 4, null, 6],
+    [1, null, 2],
+    [1, null, 2, null, null, 3],
   ].forEach( vs => {
     const root = array2bintree(vs);
     const lines0 = printTree(root).split('\n');
