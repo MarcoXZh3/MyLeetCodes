65d724533ec94b30f313d524eee80c9688a25a4c
MarcoXZh3 <marcoxzh3@gmail.com>
2019-09-02 11:58:28 -0600

    task0110 - balanced binary tree: finished

--- task0110-balanced-tree/balanced-tree.js
+++ task0110-balanced-tree/balanced-tree.js
@@ -8,7 +8,14 @@ const TreeNode = BinaryTreeNode;
  * @return {boolean}            whether the tree is balanced or not
  */
 const isBalanced = function(root) {
-
+  const depth = (root) => {
+    return root ? Math.max(depth(root.left), depth(root.right)) + 1 : 0;
+  };
+  const recursion = (root) => {
+    return !root || Math.abs(depth(root.left) - depth(root.right)) <= 1 &&
+                    recursion(root.left) && recursion(root.right);
+  };
+  return recursion(root);
 };
 
 
@@ -21,6 +28,9 @@ const main = (callback) => {
   [
     [3, 9, 20, null, null, 15, 7],
     [1, 2, 2, 3, 3, null, null, 4, 4],
+    [1, null, 2, null, null, null, 3],
+    [1, 2, 2, 3, 3, null, null, 4, 4],
+    [1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, null, null, 5, 5],
   ].forEach( vs => {
     const root = array2bintree(vs);
     console.log(`  balanced=${isBalanced(root)}`);
