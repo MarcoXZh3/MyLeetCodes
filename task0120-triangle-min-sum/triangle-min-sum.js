a96562bb1562b0a1bbed3741dda2cdc2e1cb905c
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-04 14:14:58 -0600

    task0120 - min sum in triangle: finished

--- task0120-triangle-min-sum/triangle-min-sum.js
+++ task0120-triangle-min-sum/triangle-min-sum.js
@@ -1,9 +1,16 @@
 /**
+ * dynamic programming
  * @param {number[][]}  triangle  the triangle of numbers
  * @return {number}               the minimum sum
  */
 const minimumTotal = function(triangle) {
-
+  const dp = Array(triangle.length + 1).fill(0);
+  for (let i = triangle.length - 1; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      dp[j] = Math.min(dp[j], dp[j+1]) + triangle[i][j];
+    }
+  }
+  return dp[0];
 };
 
 
@@ -20,6 +27,18 @@ const main = (callback) => {
       [6, 5, 7],
       [4, 1, 8, 3],
     ],
+    [
+      [-1],
+      [3, 2],
+      [-3, 1, -1],
+    ],
+    [
+      [7],
+      [-5, 9],
+      [6, 5, 2],
+      [-8, -2, -7, 3],
+      [-2, 6, -6,-1, 4],
+    ]
   ].forEach( vs => {
     for (let row of vs) {
       console.log(`  [${row.join(', ')}]`);
