24abab80f653ac78d69d67df34bdc6fcc8af57b0
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-04 12:00:07 -0600

    task0119 - pascal's triangle 2: finished

--- task0119-pascal-triangle-2/pascal-triangle-2.js
+++ task0119-pascal-triangle-2/pascal-triangle-2.js
@@ -1,9 +1,36 @@
 /**
+ * basic calculation
+ * @param {number}      rowIndex    index of the row in Pascal's triangle
+ * @return {number[]}               the list of numbers in the row
+ */
+const getRow0 = function(rowIndex) {
+  let re = [];
+  for (let i = 0; i <= rowIndex; i++) {
+    const prev = re;
+    re = re.slice();
+    for (let i = 1; i < re.length; i++) {
+      re[i] = prev[i - 1] + prev[i];
+    }
+    re.push(1);
+  }
+  return re;
+};
+
+
+/**
+ * O(k) calculation
  * @param {number}      rowIndex    index of the row in Pascal's triangle
  * @return {number[]}               the list of numbers in the row
  */
 const getRow = function(rowIndex) {
-  return [];
+  const re = Array(rowIndex + 1).fill(0);
+  re[0] = 1;
+  for (let i = 1; i <= rowIndex; i++) {
+    for (let j = i; j >= 1; j--) {
+      re[j] += re[j - 1];
+    }
+  }
+  return re;
 };
 
 
