333e5c19d71dc590a00c8caa80a3eee0a1a6d9a6
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-06 13:32:21 -0600

    task0124 - binary tree max path sum: finished

--- task0124-max-path-sum/max-path-sum.js
+++ task0124-max-path-sum/max-path-sum.js
@@ -8,7 +8,18 @@ const TreeNode = BinaryTreeNode;
  * @return {number}             the max path sum
  */
 const maxPathSum = function(root) {
-
+  const recursion = (root) => {
+    if (!root) {
+      return 0;
+    }
+    const left = Math.max(0, recursion(root.left));
+    const right = Math.max(0, recursion(root.right));
+    max = Math.max(max, left + right + root.val);
+    return Math.max(left, right) + root.val;
+  };
+  let max = Number.MIN_SAFE_INTEGER;
+  recursion(root);
+  return max;
 };
 
 
@@ -21,6 +32,9 @@ const main = (callback) => {
   [
     [1, 2, 3],
     [-10, 9, 20, null, null, 15, 7],
+    [2, -1],
+    [1, 2, null, null, 3, null, null, null, null, null, 4, null,
+     null, null, null, null, null, null, null, null, null, null, 5],
   ].forEach( vs => {
     const root = array2bintree(vs);
     console.log(printTree(root));
