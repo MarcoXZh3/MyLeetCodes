11bf4247c04aa52124f2a5a9dbd417c91de376e2
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-12 17:18:45 -0600

    task0131 - palindrome partition: finished

--- task0131-palindrome-partition/palindrome-partition.js
+++ task0131-palindrome-partition/palindrome-partition.js
@@ -1,9 +1,44 @@
 /**
+ * check whether the string is palindrome or not
+ * @param {string}    s     string to check
+ * @returns {boolean}       whether is palindrome or not
+ */
+const isPalindrome = function(s) {
+  for (let i = 0; i < s.length - 1 - i; i++) {
+    if (s.charAt(i) !== s.charAt(s.length - 1 - i)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+/**
  * @param {string}        s     the string to partition
  * @return {string[][]}         the partition results
  */
 const partition = function(s) {
-  return [];
+  const backtrack = (s, cur, res) => {
+    if (s.length <= 1) {
+      res.push(cur.slice());
+      if (s.length === 1) {
+        res[res.length - 1].push(s);
+      }
+      return ;
+    }
+    for (let i = 1; i <= s.length; i++) {
+      const s1 = s.substring(0, i);
+      if (!isPalindrome(s1)) {
+        continue ;
+      }
+      cur.push(s1);
+      backtrack(s.substring(i), cur, res);
+      cur.pop();
+    }
+  };
+  const res = [];
+  backtrack(s, [], res);
+  return res;
 };
 
 
@@ -15,6 +50,7 @@ const main = (callback) => {
   console.log('Task 0131 - Palindrome Partitioning:');
   [
     'aab',
+    'bb',
   ].forEach( v => {
     console.log(`  str="${v}"`);
     const strs = partition(v);
