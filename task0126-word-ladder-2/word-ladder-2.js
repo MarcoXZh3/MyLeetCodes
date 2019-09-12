a59b40611f000e1499befa68fd966b07420447e2
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-12 11:29:58 -0600

    task0126 - word ladder 2: finished

--- task0126-word-ladder-2/word-ladder-2.js
+++ task0126-word-ladder-2/word-ladder-2.js
@@ -1,11 +1,91 @@
+const buildGraph = function(beginWord, endWord, graph, dict) {
+  const queue = [beginWord];
+  const visited = {};
+  let toVisit = {};
+  toVisit[beginWord] = 1;
+  let finished = false;
+  while (queue.length > 0) {
+    for (let k in toVisit) {
+      visited[k] = 1;
+    }
+    toVisit = {};
+    const count = queue.length;
+    for (let i = 0; i < count; i++) {
+      const word = queue.shift();
+      for (let child of getNextLevel(word, dict)) {
+        if (child === endWord) {
+          finished = true;
+        }
+        if (!visited[child]) {
+          if (graph[word]) {
+            graph[word].push(child);
+          } else {
+            graph[word] = [child];
+          }
+        }
+        if (!visited[child] && !toVisit[child]) {
+          queue.push(child);
+          toVisit[child] = 1;
+        }
+      }
+    }
+    if (finished) {
+      break ;
+    }
+  }
+};
+
+const getNextLevel = function(word, dict) {
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+  const re = [];
+  for (let i = 0; i < word.length; i++) {
+    const char0 = word.charAt(i);
+    for (let j = 0; j < letters.length; j++) {
+      const char1 = letters.charAt(j);
+      if (char0 === char1) {
+        continue ;
+      }
+      const next = word.substring(0, i) + char1 + word.substring(i + 1);
+      if (dict[next]) {
+        re.push(next);
+      }
+    }
+  }
+  return re;
+};
+
+
+const dfs = function(cur, endWord, graph, path, res) {
+  path.push(cur);
+  if (cur === endWord) {
+    res.push(path.slice());
+  } else if (graph[cur]) {
+    for (let next of graph[cur]) {
+      dfs(next, endWord, graph, path, res);
+    }
+  }
+  path.pop();
+};
+
+
 /**
+ * BFS to find the shortest distance and stave to map
+ * DFS to output paths with the same shortest distance from map
  * @param {string}    beginWord   the 1st word
  * @param {string}    endWord     the 2nd word
  * @param {string[]}  wordList    the dictionary
  * @return {string[][]}           all shortest solutions
  */
 const findLadders = function(beginWord, endWord, wordList) {
-  return [];
+  const res = [];
+  const graph = {};
+  const dict = {};
+  for (let word of wordList) {
+    dict[word] = 1;
+  }
+  buildGraph(beginWord, endWord, graph, dict);
+  dfs(beginWord, endWord, graph, [], res);
+  return res;
 };
 
 
