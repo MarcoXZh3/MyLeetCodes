1708068a7954fa1aa1765fb3b30f84f6378fc4e1
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-13 12:54:44 -0600

    task0132 - palindrome partition 2: finished

--- task0132-palindrome-partition-2/palindrome-partition-2.js
+++ task0132-palindrome-partition-2/palindrome-partition-2.js
@@ -1,24 +1,24 @@
 /**
- * check whether the string is palindrome or not
- * @param {string}    s     string to check
- * @returns {boolean}       whether is palindrome or not
- */
-const isPalindrome = function(s) {
-  for (let i = 0; i < s.length - 1 - i; i++) {
-    if (s.charAt(i) !== s.charAt(s.length - 1 - i)) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-/**
+ * dynamic programming:
+ * 1. cut[i] is the minimum of cut[j - 1] + 1 (j <= i), if [j, i] is palindrome.
+ * 2. If [j, i] is palindrome, [j + 1, i - 1] is palindrome, and c[j] == c[i].
  * @param {string}    s   the string to cut to palindromes
  * @return {number}       minimum times of cutting
  */
 const minCut = function(s) {
-
+  const dp = [...Array(s.length).keys()].map(_=>[...Array(s.length).keys()].map(_=>false));
+  const cut = Array(s.length).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    let min = i;
+    for (let j = 0; j <= i; j++) {
+      if (s.charAt(i) === s.charAt(j) && (j + 1 > i - 1 || dp[j + 1][i - 1])) {
+        dp[j][i] = true;
+        min = j === 0 ? 0 : Math.min(min, cut[j - 1] + 1);
+      }
+    }
+    cut[i] = min;
+  }
+  return cut[s.length - 1];
 };
 
 
