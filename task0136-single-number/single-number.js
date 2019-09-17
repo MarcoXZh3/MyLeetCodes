19cd0388299e9ae245a346065338616005ec2222
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-17 12:51:35 -0600

    task0136 - single number: finished

--- task0136-single-number/single-number.js
+++ task0136-single-number/single-number.js
@@ -1,9 +1,33 @@
 /**
+ * hash map
+ * @param {number[]}  nums    the number array
+ * @return {number}           the single number
+ */
+const singleNumber0 = function(nums) {
+  const cnts = {};
+  for (let n of nums) {
+    cnts[`${n}`] = (cnts[`${n}`] || 0) + 1;
+  }
+  for (let k in cnts) {
+    if (cnt[k] === 1) {
+      return parseInt(k, 10);
+    }
+  }
+  return null;
+};
+
+
+/**
+ * XOR: a XOR a = 0; a XOR 0 = a
  * @param {number[]}  nums    the number array
  * @return {number}           the single number
  */
 const singleNumber = function(nums) {
-
+  let re = 0;
+  for (let n of nums) {
+    re = re ^= n;
+  }
+  return re;
 };
 
 
