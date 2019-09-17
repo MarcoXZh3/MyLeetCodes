8c0d8484ea6135dc74e52e993a8fc0f06aa5dd2e
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-17 13:02:19 -0600

    task0137 - single number II: finished

--- task0137-single-number-2/single-number-2.js
+++ task0137-single-number-2/single-number-2.js
@@ -1,9 +1,35 @@
 /**
+ * hash map
+ * @param {number[]}  nums    the number array
+ * @return {number}           the single number
+ */
+const singleNumber0 = function(nums) {
+  const cnts = {};
+  for (let n of nums) {
+    cnts[`${n}`] = (cnts[`${n}`] || 0) + 1;
+  }
+  for (let k in cnts) {
+    if (cnts[k] === 1) {
+      return parseInt(k, 10);
+    }
+  }
+  return null;
+};
+
+
+/**
+ * bitwise calculation
  * @param {number[]}  nums    the number array
  * @return {number}           the single number
  */
 const singleNumber = function(nums) {
-
+  let re = 0;
+  let tmp = 0;
+  for (let n of nums) {
+    re = (re ^ n) & ~tmp;
+    tmp = (tmp ^ n) & ~re;
+  }
+  return re;
 };
 
 
