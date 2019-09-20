3cc73bc44fb366fe50b973ed91ec9c41c5558e9b
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-20 10:01:37 -0600

    task0139 - word break: finished

--- task0139-word-break/word-break.js
+++ task0139-word-break/word-break.js
@@ -4,7 +4,17 @@
  * @return {boolean}                whether the string can be split or not
  */
 const wordBreak = function(s, wordDict) {
-
+  const dp = Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDict.includes(s.substring(j, i))) {
+        dp[i] = true;
+        break ;
+      }
+    }
+  }
+  return dp[s.length];
 };
 
 
@@ -18,9 +28,15 @@ const main = (callback) => {
     ['leetcode', ['leet', 'code']],
     ['applepenapple', ['apple', 'pen']],
     ['catsandog', ['cats', 'dog', 'sand', 'and', 'cat']],
+    ['abcd', ['a','abc','b','cd']],
+    ['cars', ['car','ca','rs']],
+    ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' +
+     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' +
+     'aaaaaaaab',
+     ['a','aa','aaa','aaaa','aaaaa','aaaaaa','aaaaaaa','aaaaaaaa','aaaaaaaaa','aaaaaaaaaa']],
   ].forEach( vs => {
-    console.log(`  dict=[${vs[1].map( s=>`"${s}"`).join(', ')}]`);
-    console.log(`  re=${wordBreak(vs[0], vs[1])}, str="${vs[0]}"`);
+    console.log(`  dict=[${vs[1].map( s=>`'${s}'`).join(', ')}]`);
+    console.log(`  re=${wordBreak(vs[0], vs[1])}, str='${vs[0]}'`);
   });
   if (callback) {
     callback();
