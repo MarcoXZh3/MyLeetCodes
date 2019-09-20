d7172ac9420f628907b4e0f217b485b502a442bf
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-20 12:14:50 -0600

    task0140 - word break 2: finished

--- task0140-word-break-2/word-break-2.js
+++ task0140-word-break-2/word-break-2.js
@@ -1,10 +1,29 @@
 /**
+ * DFS
  * @param {string}      s           the string to split
  * @param {string[]}    wordDict    the dictionary
  * @return {string[]}               all possible solutions
  */
 const wordBreak = function(s, wordDict) {
-  return [];
+  const dfs = (s, wordDict, map) => {
+    if (map[s]) {
+      return map[s];
+    } else if (s.length === 0) {
+      return [s];
+    }
+    const res = [];
+    for (let word of wordDict) {
+      if (!s.startsWith(word)) {
+        continue ;
+      }
+      for (let sub of dfs(s.substring(word.length), wordDict, map)) {
+        res.push((word + ' ' + sub).trim());
+      }
+    }
+    map[s] = res;
+    return res;
+  };
+  return dfs(s, wordDict, {});
 };
 
 
