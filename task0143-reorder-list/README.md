b8721f124187322883e90afdbf203f698adf3432
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-23 14:38:53 -0600

    task0143 - reorder list: finished

--- task0143-reorder-list/README.md
+++ task0143-reorder-list/README.md
@@ -2,7 +2,7 @@
 
 \<Medium>
 
-Given a singly linked list `L`: `L0 -> L1 -> ... => -> Ln`, reorder it to:
+Given a singly linked list `L`: `L0 -> L1 -> ... -> Ln`, reorder it to:
 `L0 -> Ln -> L1 -> L(n-1) -> L2 -> L(n-2) -> ...`. You may not modify the values
 in the list's nodes, only nodes itself may be changed.
 
--- task0143-reorder-list/reorder-list.js
+++ task0143-reorder-list/reorder-list.js
@@ -6,8 +6,63 @@ const { ListNode, array2list, printList } = require(path.resolve('_utils/list'))
  * @param {ListNode}    head    head of the linked list
  * @return {void} Do not return anything, modify head in-place instead.
  */
-const reorderList = function(head) {
+const reorderList0 = function(head) {
+  let cur = head;
+  while (cur) {
+    let prev = null;
+    let tail = cur;
+    while (tail.next) {
+      prev = tail;
+      tail = tail.next;
+    }
+    if (cur === tail || cur === prev) {
+      break ;
+    }
+    const next = cur.next;
+    cur.next = tail;
+    tail.next = next;
+    prev.next = null;
+    cur = next;
+  }
+};
 
+
+/**
+ * reverse 2nd half and reorder the two halves
+ * @param {ListNode}    head    head of the linked list
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+const reorderList = function(head) {
+  if (!head || !head.next) {
+    return ;
+  }
+  let p1 = head;
+  let p2 = head;
+  while (p2.next && p2.next.next) {
+    p1 = p1.next;
+    p2 = p2.next.next;
+  }
+
+  // reverse the 2nd half so 1->2->3->4->5 becomes 1->2->3->5->4
+  const mid = p1;
+  const next = p1.next;
+  while (next.next) {
+    const cur = next.next;
+    next.next = cur.next;
+    cur.next = mid.next;
+    mid.next = cur;
+  }
+
+  // reorder the two parts
+  p1 = head;
+  p2 = mid.next;
+  while (p1 !== mid) {
+    mid.next = p2.next;
+    p2.next = p1.next;
+    p1.next = p2;
+    p1 = p2.next;
+    p2 = mid.next;
+  }
 };
 
 
