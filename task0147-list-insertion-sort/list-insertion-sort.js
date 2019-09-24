f355282c07de70f02cb7b646e7dd399596bb17ed
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-24 15:56:13 -0600

    task0147 - list by insertion sort: finished

--- task0147-list-insertion-sort/list-insertion-sort.js
+++ task0147-list-insertion-sort/list-insertion-sort.js
@@ -7,7 +7,24 @@ const { ListNode, array2list, printList } = require(path.resolve('_utils/list'))
  * @return {ListNode}           the sorted list's head
  */
 const insertionSortList = function(head) {
-  return head;
+  const dummy = new ListNode(null);
+  while (head) {
+    // grab a node from the original list -- the head
+    const cur = head;
+    head = head.next;
+
+    // find the right position to insert the node
+    let prev = dummy;
+    while (prev.next && prev.next.val < cur.val) {
+      prev = prev.next;
+    }
+
+    // insert the node between the target position
+    const next = prev.next;
+    prev.next = cur;
+    cur.next = next;
+  }
+  return dummy.next;
 };
 
 
