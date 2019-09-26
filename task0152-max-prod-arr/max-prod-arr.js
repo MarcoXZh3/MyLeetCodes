a5dbfb2e6280e861ed03414713df746c270e1bf4
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-26 09:57:34 -0600

    task0152 - max product subarray: finished

--- task0152-max-prod-arr/max-prod-arr.js
+++ task0152-max-prod-arr/max-prod-arr.js
@@ -1,9 +1,41 @@
 /**
+ * brutal force: check every possible subarray
+ * @param {number[]}  nums    the number array
+ * @return {number}           the max product
+ */
+const maxProduct0 = function(nums) {
+  let re = nums[0];
+  for (let i = 0; i < nums.length; i++) {
+    let max = 1;
+    for (let j = i; j < nums.length; j++) {
+      max *= nums[j];
+      re = Math.max(re, max);
+      if (nums[j] === 0) {
+        break ;
+      }
+    }
+  }
+  return re;
+};
+
+
+/**
  * @param {number[]}  nums    the number array
  * @return {number}           the max product
  */
 const maxProduct = function(nums) {
-
+  let re = nums[0];
+  for (let i = 1, max = re, min = re; i < nums.length; i++) {
+    if (nums[i] < 0) {
+      const tmp = max;
+      max = min;
+      min = tmp;
+    }
+    max = Math.max(nums[i], max * nums[i]);
+    min = Math.min(nums[i], min * nums[i]);
+    re = Math.max(re, max);
+  }
+  return re;
 };
 
 
