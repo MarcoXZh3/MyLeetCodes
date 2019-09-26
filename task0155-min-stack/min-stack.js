557f9b780fc040f6a3a5016a7b482aa2f7217231
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-09-26 11:41:09 -0600

    task0155 - min stack: finished

--- task0155-min-stack/min-stack.js
+++ task0155-min-stack/min-stack.js
@@ -1,41 +1,120 @@
 /**
- * initialize your data structure here.
+ * The min stack which allows to peek the minimum element
+ */
+const MinStack0 = function() {
+  /**
+   * attributes
+   */
+  const vals = [];
+  const orders = [];
+
+  /**
+   * push element to the stack
+   * @param {object}    x     element to push
+   */
+  this.push = (x) => {
+    vals.push(x);
+    orders.push(x);
+    orders.sort( (a,b)=>a-b );
+  };
+  /**
+   * pop element from the stack
+   * @return {object}         element to pop
+   */
+  this.pop = () => {
+    if (vals.length === 0) {
+      return null;
+    }
+    const x = vals.pop();
+    const idx = orders.indexOf(x);
+    orders.splice(idx, 1);
+    return x;
+  };
+  /**
+   * peek the pop element of the stack
+   * @return {object}         element on the top
+   */
+  this.top = () => {
+    return vals.length > 0 ? vals[vals.length - 1] : null;
+  };
+  /**
+   * peek the minimum element in the stack
+   * @return {object}         the minimum element
+   */
+  this.getMin = () => {
+    return orders.length > 0 ? orders[0] : null;
+  };
+  /**
+   * cast the stack to a string
+   * @return {object}         element to push
+   */
+  this.toString = () => {
+    const size = vals.length;
+    let str = `MinStack{size=${size},min=${size>0?orders[0]:null}}:`;
+    str += ` >>>[${vals.slice().reverse().join(', ')}]`;
+    // str += ` [${orders.join(', ')}]`;
+    return str;
+  };
+};
+
+
+/**
+ * The min stack which allows to peek the minimum element
  */
 const MinStack = function() {
-  this.toString = () => 'MinStack';
-};
+  /**
+   * attributes
+   */
+  let min = Number.MAX_SAFE_INTEGER;
+  const stack = [];
 
-
-/** 
- * @param {number} x
- * @return {void}
- */
-MinStack.prototype.push = function(x) {
-
-};
-
-
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-
-};
-
-
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-
-};
-
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-
+  /**
+   * push element to the stack
+   * @param {object}    x     element to push
+   */
+  this.push = (x) => {
+    // if min is about to change, cache the old min before actual pushing
+    if (x <= min) {
+      stack.push(min);
+      min = x;
+    }
+    stack.push(x);
+  };
+  /**
+   * pop element from the stack
+   * @return {object}         element to pop
+   */
+  this.pop = () => {
+    if (stack.length === 0) {
+      return null;
+    }
+    const x = stack.pop();
+    // if min is just popped, the top element will be the previous min
+    if (x === min) {
+      min = stack.pop();
+    }
+  };
+  /**
+   * peek the pop element of the stack
+   * @return {object}         element on the top
+   */
+  this.top = () => {
+    return stack.length > 0 ? stack[stack.length - 1] : null;
+  };
+  /**
+   * peek the minimum element in the stack
+   * @return {object}         the minimum element
+   */
+  this.getMin = () => {
+    return min;
+  };
+  /**
+   * cast the stack to a string
+   * @return {object}         element to push
+   */
+  this.toString = () => {
+    return `MinStack{min=${min}, top=${this.top()}}:`
+  };
 };
 
 
@@ -45,8 +124,10 @@ MinStack.prototype.getMin = function() {
  */
 const main = (callback) => {
   console.log('Task 0155 - Min Stack:');
-  const minStack = new MinStack();
-  console.log(`  const minStack = new MinStack();`);
+  let minStack;
+
+  minStack = new MinStack();
+  console.log(`  minStack = new MinStack();`);
   minStack.push(-2);
   console.log(`  minStack.push(-2);   // ${minStack.toString()}`);
   minStack.push(0);
@@ -58,6 +139,28 @@ const main = (callback) => {
   console.log(`  minStack.pop();      // ${minStack.toString()}`);
   console.log(`  minStack.top();      // returns ${minStack.top()}`);
   console.log(`  minStack.getMin();   // returns ${minStack.getMin()}`);
+
+  minStack = new MinStack();
+  console.log(`  minStack = new MinStack();`);
+  minStack.push(2);
+  console.log(`  minStack.push(2);    // ${minStack.toString()}`);
+  minStack.push(0);
+  console.log(`  minStack.push(0);    // ${minStack.toString()}`);
+  minStack.push(3);
+  console.log(`  minStack.push(3);    // ${minStack.toString()}`);
+  minStack.push(0);
+  console.log(`  minStack.push(0);    // ${minStack.toString()}`);
+  console.log(`  minStack.getMin();   // returns ${minStack.getMin()}`);
+  minStack.pop();
+  console.log(`  minStack.pop();      // ${minStack.toString()}`);
+  console.log(`  minStack.getMin();   // returns ${minStack.getMin()}`);
+  minStack.pop();
+  console.log(`  minStack.pop();      // ${minStack.toString()}`);
+  console.log(`  minStack.getMin();   // returns ${minStack.getMin()}`);
+  minStack.pop();
+  console.log(`  minStack.pop();      // ${minStack.toString()}`);
+  console.log(`  minStack.getMin();   // returns ${minStack.getMin()}`);
+
   if (callback) {
     callback();
   }
