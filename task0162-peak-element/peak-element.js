0eb889713343fa444ca8b22c6950714c74f6ce56
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-02 11:02:34 -0600

    task0162 - peak element: finished

--- task0162-peak-element/peak-element.js
+++ task0162-peak-element/peak-element.js
@@ -1,9 +1,54 @@
 /**
+ * linear search
+ * @param {number[]}    nums    the number array
+ * @return {number}             the index of a peak element
+ */
+const findPeakElement0 = function(nums) {
+  let i = 0;
+  while (i < nums.length) {
+    if ( (i === 0 || nums[i - 1] < nums[i]) &&
+         (i === nums.length - 1 || nums[i] > nums[i + 1]) ) {
+      break ;
+    }
+    i ++;
+  }
+  return i;
+};
+
+
+/**
+ * binary search - recursion
+ * @param {number[]}    nums    the number array
+ * @return {number}             the index of a peak element
+ */
+const findPeakElement1 = function(nums) {
+  const recursion = (l, r) => {
+    const mid = Math.floor((l + r) / 2);
+    return l === r ? l : (
+              nums[mid] > nums[mid + 1] ? recursion(l, mid) : recursion(mid + 1, r)
+           );
+  };
+  return recursion(0, nums.length - 1);
+};
+
+
+/**
+ * binary search - iteration
  * @param {number[]}    nums    the number array
  * @return {number}             the index of a peak element
  */
 const findPeakElement = function(nums) {
-
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const mid = Math.floor((l + r) / 2);
+    if (nums[mid] > nums[mid + 1]) {
+      r = mid;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return l;
 };
 
 
