011a3153b951d6b95cfd61cfda7e37d38a890b93
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 16:54:44 -0600

    task0196 - <SQL> delete duplicated emails: finished

--- task0196-del-dup-emails/del-dup-emails.sql
+++ task0196-del-dup-emails/del-dup-emails.sql
@@ -1,1 +1,21 @@
 -- Task 0196 - <SQL> Delete Duplicate Emails
+
+-- WITH cnts AS
+-- (
+--   SELECT
+--     ROW_NUMBER() OVER (PARTITION BY Email ORDER BY Id) AS cnt
+--   FROM Person WITH (NOLOCK)
+-- )
+-- DELETE FROM cnts
+-- WHERE
+--   cnts.cnt > 1
+-- ;
+
+DELETE p1 FROM
+  Person AS p1 WITH (NOLOCK),
+  Person AS p2 WITH (NOLOCK)
+WHERE
+  p1.Email = p2.Email AND p1.Id > p2.Id
+;
+
+SELECT * FROM Person;
