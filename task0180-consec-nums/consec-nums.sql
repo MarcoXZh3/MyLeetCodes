ba614a010a532168d61ce888eb275b37e923db50
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 14:19:48 -0600

    task0180 - <SQL> consecutive numbers: finished

--- task0180-consec-nums/consec-nums.sql
+++ task0180-consec-nums/consec-nums.sql
@@ -1,1 +1,12 @@
 -- Task 0180 - <SQL> Consecutive Numbers
+SELECT
+  DISTINCT
+  l1.Num AS ConsecutiveNums
+FROM
+  Logs AS l1 WITH (NOLOCK),
+  Logs AS l2 WITH (NOLOCK),
+  Logs AS l3 WITH (NOLOCK)
+WHERE
+  l1.Num = l2.Num AND l1.Id + 1 = l2.Id AND
+  l2.Num = l3.Num AND l2.Id + 1 = l3.Id
+;
