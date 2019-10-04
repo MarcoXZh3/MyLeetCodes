ba5c2e9f598c1a071e688298342b774553b97913
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 13:52:08 -0600

    task0177 - <SQL> nth highest salary: finished

--- task0177-nth-high-salary/nth-high-salary.sql
+++ task0177-nth-high-salary/nth-high-salary.sql
@@ -1,5 +1,15 @@
 -- Task 0177 - <SQL> Nth Highest Salary
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
-  RETURN NULL
+  RETURN
+  (
+    SELECT
+      DISTINCT
+      e.Salary
+    FROM Employee AS e WITH (NOLOCK)
+    ORDER BY
+      e.Salary DESC
+    OFFSET (@N - 1) ROWS
+    FETCH NEXT 1 ROW ONLY
+  );
 END
