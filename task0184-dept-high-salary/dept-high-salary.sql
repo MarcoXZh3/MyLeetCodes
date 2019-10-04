f45d979b18726836d4b41dcf07ef0296de9f4259
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 15:43:56 -0600

    task0184 - <SQL> department highest salaries: finished

--- task0184-dept-high-salary/dept-high-salary.sql
+++ task0184-dept-high-salary/dept-high-salary.sql
@@ -1,1 +1,20 @@
 -- Task 0184 - <SQL> Department Highest Salary
+WITH maxSalaries AS (
+  SELECT
+    d.Id,
+    d.Name        AS Department,
+    MAX(e.Salary) AS Salary
+  FROM Employee AS e WITH (NOLOCK)
+  INNER JOIN Department AS d WITH (NOLOCK) ON e.DepartmentId = d.Id
+  GROUP BY
+    d.Id,
+    d.Name
+)
+SELECT
+  ms.Department,
+  e.Name  AS Employee,
+  ms.Salary
+FROM Employee AS e WITH (NOLOCK)
+INNER JOIN maxSalaries AS ms WITH (NOLOCK) ON ms.Id = e.DepartmentId
+                                          AND e.Salary = ms.Salary
+;
