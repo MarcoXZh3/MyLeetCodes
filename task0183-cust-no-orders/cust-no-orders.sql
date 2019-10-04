8feb1267c33400b47e1b26c62d9648a1bc22aea3
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 14:44:33 -0600

    task0183 - <SQL> customers without orders: finished

--- task0183-cust-no-orders/cust-no-orders.sql
+++ task0183-cust-no-orders/cust-no-orders.sql
@@ -1,1 +1,8 @@
 -- Task 0183 - <SQL> Customers Who Never Order
+SELECT
+  c.Name AS Customers
+FROM Customers AS c WITH (NOLOCK)
+LEFT JOIN Orders AS o WITH (NOLOCK) ON c.Id = o.CustomerId
+WHERE
+  o.Id IS NULL
+;
