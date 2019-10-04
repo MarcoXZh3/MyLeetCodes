7eaff3a31746fbecdaee3a4352c917912d4b7e8b
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 13:23:32 -0600

    task0175 - <SQL> combine two tables: finished

--- task0175-combine-tables/combine-tables.sql
+++ task0175-combine-tables/combine-tables.sql
@@ -1,1 +1,9 @@
 -- Task 0175 - <SQL> Combine Two Tables
+SELECT
+  p.FirstName,
+  p.LastName,
+  a.City,
+  a.State
+FROM Person AS p WITH (NOLOCK)
+LEFT JOIN [Address] AS a WITH (NOLOCK) ON p.PersonId = a.PersonId
+;
