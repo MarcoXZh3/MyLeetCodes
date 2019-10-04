71df537fe958780cdd6af6d33f78aa9e9514f7fe
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 17:03:53 -0600

    task0197 - <SQL> rising temperatures: finished

--- task0197-raise-temp/raise-temp.sql
+++ task0197-raise-temp/raise-temp.sql
@@ -1,1 +1,10 @@
 -- Task 0197 - <SQL> Rising Temperature
+SELECT
+  t2.Id
+FROM
+  Weather AS t1 WITH (NOLOCK),
+  Weather AS t2 WITH (NOLOCK)
+WHERE
+  DATEADD(DD, 1, t1.RecordDate) = t2.RecordDate AND
+  t2.Temperature > t1.Temperature
+;
