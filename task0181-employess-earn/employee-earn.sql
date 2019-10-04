d1085ff598adbe262527f06422c9c30b8b3acde6
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 14:31:05 -0600

    task0181 - <SQL> employee earnings: finished

--- task0181-employess-earn/employee-earn.sql
+++ task0181-employess-earn/employee-earn.sql
@@ -1,1 +1,8 @@
 -- Task 0181 - <SQL> Employees Earning More Than Their Managers
+SELECT
+  e.[Name] AS Employee
+FROM Employee AS e WITH (NOLOCK)
+INNER JOIN Employee AS m WITH (NOLOCK) ON e.ManagerId = m.Id
+WHERE
+  e.Salary > m.Salary
+;
