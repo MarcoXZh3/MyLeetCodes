3fe3f344a728c5847c8e17ff3718a0ee0b80877e
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 13:38:46 -0600

    task0176 - <SQL> 2nd highest salary: finished

--- task0176-2nd-high-salary/2nd-high-salary.sql
+++ task0176-2nd-high-salary/2nd-high-salary.sql
@@ -1,1 +1,13 @@
 -- Task 0176 - <SQL> Second Highest Salary
+SELECT
+(
+  SELECT
+    DISTINCT
+    e.Salary
+  FROM Employee AS e WITH (NOLOCK)
+  ORDER BY
+    e.Salary DESC
+  OFFSET 1 ROW
+  FETCH NEXT 1 ROW ONLY
+) AS SecondHighestSalary
+;
