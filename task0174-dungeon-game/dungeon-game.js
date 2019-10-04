14142c2713c12907c939a18b40a52bd4e2818cdb
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-04 13:00:24 -0600

    task0174 - dungeon game: finished

--- task0174-dungeon-game/dungeon-game.js
+++ task0174-dungeon-game/dungeon-game.js
@@ -3,7 +3,18 @@
  * @return {number}                 minimum health to pass the dungeon
  */
 const calculateMinimumHP = function(dungeon) {
-
+  const m = dungeon.length;
+  const n = dungeon[0].length;
+  const dp = [...Array(m + 1).keys()].map(_=>[...Array(n + 1).keys()].map(_=>Number.MAX_SAFE_INTEGER));
+  dp[m][n - 1] = 1;
+  dp[m - 1][n] = 1;
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const hp = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+      dp[i][j] = hp <= 0 ? 1 : hp;
+    }
+  }
+  return dp[0][0];
 };
 
 
