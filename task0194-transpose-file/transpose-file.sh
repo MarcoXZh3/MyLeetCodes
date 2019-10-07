d7e8d76e843dc3a923e826351511b150d7fa60b9
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-07 14:29:04 -0600

    task0194 - <BASH> transpose file: finished

--- task0194-transpose-file/transpose-file.sh
+++ task0194-transpose-file/transpose-file.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 # Task 0194 - <BASH> Transpose File
+
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if(NR == 1) {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++) {
+        print s[i];
+    }
+}' file.txt
