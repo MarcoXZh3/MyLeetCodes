0d7ca56ce80a88e75b9cc7cd7e05ffd0c0c94b7a
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-07 14:16:01 -0600

    task0192 - <BASH> word frequency: finished

--- task0192-word-freq/word-freq.sh
+++ task0192-word-freq/word-freq.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
 # Task 0192 - <BASH> Word Frequency
+
+
+# `cat`: read the textual file as string
+# `tr -s`: split into words, 1 word per each line
+# `sort`: sort the words/lines
+# `uniq -c`: count how many time each unique word occures
+# `sort -r`: sort by occurances, in descending order
+# `awk '{ print $2, $1 }'`: format the output
+cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -r | awk '{ print $2, $1 }'
