1307d4d5eca1acab14ef3712e713224cef7c2a1f
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-08 11:23:38 -0600

    task0198 - house robber: finished

--- task0198-house-robber/house-robber.js
+++ task0198-house-robber/house-robber.js
@@ -1,9 +1,33 @@
 /**
+ * dynamic programming with O(n) space
+ * @param {number[]}    nums    cashes in houses
+ * @return {number}             max amount to grab
+ */
+const rob0 = function(nums) {
+  const dp = Array(nums.length).fill(0);
+  for (let i = 0; i < nums.length; i++) {
+    dp[i] = nums[i] + Math.max(dp[i - 2] || 0, dp[i - 3] || 0);
+  }
+  return Math.max(dp[dp.length - 1] || 0, dp[dp.length - 2] || 0);
+};
+
+
+/**
+ * dynamic programming with O(1) space
  * @param {number[]}    nums    cashes in houses
  * @return {number}             max amount to grab
  */
 const rob = function(nums) {
-
+  let re1 = 0;
+  let re2 = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      re1 = Math.max(re1 + nums[i], re2);
+    } else {
+      re2 = Math.max(re1, re2 + nums[i]);
+    }
+  }
+  return Math.max(re1, re2);
 };
 
 
@@ -16,6 +40,7 @@ const main = (callback) => {
   [
     [1, 2, 3, 1],
     [2, 7, 9, 3, 1],
+    [2, 1, 1, 2],
   ].forEach( vs => {
     console.log(`  max=${rob(vs)}, nums=[${vs.join(', ')}]`);
   });
