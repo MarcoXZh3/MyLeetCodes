9166d02820e0694107151784c6d7aba9a3c3467c
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-08 12:00:47 -0600

    task0199 - binary tree right side view: finished

--- task0199-bin-tree-right-view/bin-tree-right-view.js
+++ task0199-bin-tree-right-view/bin-tree-right-view.js
@@ -8,7 +8,21 @@ const TreeNode = BinaryTreeNode;
  * @return {number[]}           the traversal results
  */
 const rightSideView = function(root) {
-  return [];
+  const recursion = (root, level, res) => {
+    if (!root) {
+      return ;
+    }
+    if (level < res.length) {
+      res[level] = root.val;
+    } else {
+      res.push(root.val);
+    }
+    recursion(root.left, level + 1, res);
+    recursion(root.right, level + 1, res);
+  };
+  const res = [];
+  recursion(root, 0, res);
+  return res;
 };
 
 
@@ -19,7 +33,9 @@ const rightSideView = function(root) {
 const main = (callback) => {
   console.log('Task 0199 - Binary Tree Right Side View:');
   [
-    [1, null, 2, null, null, 3],
+    [1, 2, 3, null, 5, null, 4],
+    [1, 2],
+    [1, 2, 3, 4],
   ].forEach( vs => {
     const root = array2bintree(vs);
     console.log(`right=[${rightSideView(root).join(', ')}]`);
