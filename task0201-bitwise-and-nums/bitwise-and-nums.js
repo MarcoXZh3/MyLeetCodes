d61a77e9823911caeda9730dc1d07b2848f93911
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-09 14:32:53 -0600

    task0201 - bitwise and of number ranges: finished

--- task0201-bitwise-and-nums/bitwise-and-nums.js
+++ task0201-bitwise-and-nums/bitwise-and-nums.js
@@ -3,9 +3,22 @@
  * @param {number}      n   last number of the range
  * @return {number}         bitwise and of all the numbers
  */
-const rangeBitwiseAnd = function(m, n) {
-
+const rangeBitwiseAnd0 = function(m, n) {
+  while(m < n) {
+    n = n & (n-1);
+  }
+  return n;
 };
+
+
+/**
+ * @param {number}      m   first number of the range
+ * @param {number}      n   last number of the range
+ * @return {number}         bitwise and of all the numbers
+ */
+const rangeBitwiseAnd = function(m, n) {
+  return n > m ? (rangeBitwiseAnd(Math.floor(m / 2), Math.floor(n / 2)) << 1) : m;
+}
 
 
 /**
@@ -17,6 +30,7 @@ const main = (callback) => {
   [
     [5, 7],
     [0, 1],
+    [0, 2147483647],
   ].forEach( vs => {
     console.log(`  re=${rangeBitwiseAnd(vs[0], vs[1])}, range=[${vs[0]}, ${vs[1]}]`);
   });
