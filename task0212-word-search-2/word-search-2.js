6a0f829fe8b7372205dda24bc3e7bc5f68dee4ce
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-17 16:46:24 -0600

    task0212 - word search II: finished

--- task0212-word-search-2/word-search-2.js
+++ task0212-word-search-2/word-search-2.js
@@ -4,7 +4,40 @@
  * @return {string[]}             the filtered words
  */
 const findWords = function(board, words) {
-  return words;
+  const dfs = function(board, i, j, word) {
+    // check the first char
+    if (board[i][j] !== word.charAt(0)) {
+      return false;
+    }
+    // mark the current char as visited
+    board[i][j] += '!';
+    // check the next chars recursively
+    const sub = word.substring(1);
+    // no more chars, or all next chars matched - fully matched
+    if (sub.length === 0 ||
+        i - 1 >= 0 && dfs(board, i - 1, j, sub) ||
+        i + 1 < board.length && dfs(board, i + 1, j, sub) ||
+        j - 1 >= 0 && dfs(board, i, j - 1, sub) ||
+        j + 1 < board[0].length && dfs(board, i, j + 1, sub)) {
+      return true;
+    }
+    // otherwise restore the current char and return not matched
+    board[i][j] = board[i][j].charAt(0);
+    return false;
+  };
+
+  // filter out the words
+  return words.filter( word => {
+    const cp = JSON.parse(JSON.stringify(board));
+    for (let i = 0; i < cp.length; i++) {
+      for (let j = 0; j < cp[0].length; j++) {
+        if (dfs(cp, i, j, word)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  });
 };
 
 
