c106a40e2e905a4c4b1b53f21e08b53fd885c5e7
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-21 14:13:56 -0600

    task0217 - has duplicates: finished

--- task0217-has-dups/has-dups.js
+++ task0217-has-dups/has-dups.js
@@ -3,7 +3,14 @@
  * @return {boolean}            whether has duplicates or not
  */
 const containsDuplicate = function(nums) {
-
+  const set = new Set();
+  for (let num of nums) {
+    if (set.has(num)) {
+      return true;
+    }
+    set.add(num);
+  }
+  return false;
 };
 
 
