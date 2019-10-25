6598870ae9ce2f588ebdae915bbbea5e7734c377
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-25 12:42:32 -0600

    task0227 - basic calculator 2: finished

--- task0227-calculator-2/calculator-2.js
+++ task0227-calculator-2/calculator-2.js
@@ -3,7 +3,34 @@
  * @return {number}           the result
  */
 const calculate = function(s) {
-
+  let stack = [];
+  let num = 0;
+  let sign = '+';
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    if (/\d/.test(char)) {
+      num = num * 10 + parseInt(char, 10);
+    }
+    if (i === s.length - 1 || /\D/.test(char) && char !== ' ') {
+      if (sign === '+') {
+        stack.push(num);
+      } else if (sign === '-') {
+        stack.push(-num);
+      } else if (sign === '*') {
+        stack.push(stack.pop() * num);
+      } else if (sign === '/') {
+        const re = stack.pop() / num;
+        stack.push(Math.floor(Math.abs(re)) * (re < 0 ? -1 : 1));
+      }
+      num = 0;
+      sign = char;
+    }
+  }
+  let re = 0;
+  while (stack.length > 0) {
+    re += stack.pop();
+  }
+  return re;
 };
 
 
