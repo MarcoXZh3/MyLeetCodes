2cb413f41f9e97a0d8f2f4d4c00045fc06308091
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-28 13:28:23 -0600

    task0234 - palindrome linked list: finished

--- task0234-palindrome-list/palindrome-list.js
+++ task0234-palindrome-list/palindrome-list.js
@@ -3,11 +3,58 @@ const { ListNode, array2list, printList } = require(path.resolve('_utils/list'))
 
 
 /**
+ * reverse the entire list by using array
+ * @param {ListNode}    head    head of the linked list
+ * @return {boolean}            whether palindrome or not
+ */
+const isPalindrome0 = function(head) {
+  const nodes = [];
+  let cur = head;
+  while (cur) {
+    nodes.push(cur);
+    cur = cur.next;
+  }
+  for (let i = 0, j = nodes.length - 1; i < j; i++, j--) {
+    if (nodes[i].val !== nodes[j].val) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+/**
+ * reverse the 2nd half only
  * @param {ListNode}    head    head of the linked list
  * @return {boolean}            whether palindrome or not
  */
 const isPalindrome = function(head) {
+  // locate the middle node
+  let fast = head;
+  let slow = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  if (fast) {
+    slow = slow.next;
+  }
 
+  // reverse the 2nd half of the list (middle to tail becomes tail to middle)
+  let rev = null;
+  while (slow) {
+    const next = slow.next;
+    slow.next = rev;
+    rev = slow;
+    slow = next;
+  }
+
+  // compare the 1st half and the reversed 2nd half
+  while (rev && head.val === rev.val) {
+    head = head.next;
+    rev = rev.next;
+  }
+  return !rev;
 };
 
 
@@ -22,7 +69,7 @@ const main = (callback) => {
     [1, 2, 2, 1],
   ].forEach( vs => {
     const list = array2list(vs);
-    console.log(`  palindrome=${isPalindrome(list)}, list=${printList(list)}`);
+    console.log(`  list=${printList(list)}, palindrome=${isPalindrome(list)}`);
   });
   if (callback) {
     callback();
