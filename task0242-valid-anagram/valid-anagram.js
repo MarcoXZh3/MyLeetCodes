ed09b1e47c10fef1de0fd9cfa68337145705ba89
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-29 15:43:20 -0600

    task0242 - valid anagram: finished

--- task0242-valid-anagram/valid-anagram.js
+++ task0242-valid-anagram/valid-anagram.js
@@ -1,10 +1,39 @@
 /**
+ * sort and compare
+ * @param {string}      s     the 1st string
+ * @param {string}      t     the 2nd string
+ * @return {boolean}          whether they are anagram or not
+ */
+const isAnagram0 = function(s, t) {
+  return s.length !== t.length ? false :
+            s.split('').sort( (a,b)=>a>b?1:-1 ).join('') ===
+            t.split('').sort( (a,b)=>a>b?1:-1 ).join('');
+};
+
+
+/**
+ * count chars and compare
  * @param {string}      s     the 1st string
  * @param {string}      t     the 2nd string
  * @return {boolean}          whether they are anagram or not
  */
 const isAnagram = function(s, t) {
-
+  if (s.length !== t.length) {
+    return false;
+  }
+  const map = {};
+  for (let i = 0; i < s.length; i++) {
+    let char = s.charAt(i);
+    map[char] = (map[char] || 0) + 1;
+    char = t.charAt(i);
+    map[char] = (map[char] || 0) - 1;
+  }
+  for (let char in map) {
+    if (map[char] !== 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 
