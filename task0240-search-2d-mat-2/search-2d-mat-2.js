3de1839c15014fdfb29ff9e2003910aa28ea7a11
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-29 13:44:03 -0600

    task0240 - search 2d matrix: finished

--- task0240-search-2d-mat-2/search-2d-mat-2.js
+++ task0240-search-2d-mat-2/search-2d-mat-2.js
@@ -1,10 +1,43 @@
 /**
+ * basic search row by column
+ * @param {number[][]}  matrix    the number matrix
+ * @param {number}      target    the number to search
+ * @return {boolean}              whether found or not
+ */
+const searchMatrix0 = function(matrix, target) {
+  for (let i = 0; i < matrix.length && matrix[i][0] <= target; i++) {
+    for (let j = 0; j < matrix[i].length && matrix[i][j] <= target; j++) {
+      if (matrix[i][j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+
+/**
+ * basic search row by column
  * @param {number[][]}  matrix    the number matrix
  * @param {number}      target    the number to search
  * @return {boolean}              whether found or not
  */
 const searchMatrix = function(matrix, target) {
-
+  if (!matrix || matrix.length === 0) {
+    return false;
+  }
+  let col = matrix[0].length - 1;
+  let row = 0;
+  while (col >= 0 && row <= matrix.length - 1) {
+    if (target === matrix[row][col]) {
+      return true;
+    } else if (target < matrix[row][col]) {
+        col --;
+    } else if (target > matrix[row][col]) {
+        row ++;
+    }
+  }
+  return false;
 };
 
 
@@ -33,6 +66,16 @@ const main = (callback) => {
         [3,   6,  9, 16, 22],
         [10, 13, 14, 17, 24],
         [18, 21, 23, 26, 30],
+      ],
+    ],
+    [
+      13,
+      [
+        [1,  3,  5,  7,  9 ],
+        [2,  4,  6,  8,  10],
+        [11, 13, 15, 17, 19],
+        [12, 14, 16, 18, 20],
+        [21, 22, 23, 24, 25],
       ],
     ],
   ].forEach( vs => {
