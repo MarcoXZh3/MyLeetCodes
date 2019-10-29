e60a349539ddf48c70680714cfe6c28d4a41c0e4
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-29 17:51:21 -0600

    task0241 - adding brackets: finished

--- task0241-add-brackets/add-brackets.js
+++ task0241-add-brackets/add-brackets.js
@@ -3,7 +3,30 @@
  * @return {number[]}           all results by adding brackets to the expression
  */
 const diffWaysToCompute = function(input) {
-  return [];
+  const res = [];
+  for (let i = 0; i < input.length; i++) {
+    const char = input.charAt(i);
+    if (!['+', '-', '*'].includes(char)) {
+      continue ;
+    }
+    const res1 = diffWaysToCompute(input.substring(0, i));
+    const res2 = diffWaysToCompute(input.substring(i + 1));
+    for (let re1 of res1) {
+      for (let re2 of res2) {
+        if (char === '+') {
+          res.push(re1 + re2);
+        } else if (char === '-') {
+          res.push(re1 - re2);
+        } else if (char === '*') {
+          res.push(re1 * re2);
+        }
+      }
+    }
+  }
+  if (res.length === 0) {
+    res.push(parseInt(input, 10));
+  }
+  return res;
 };
 
 
