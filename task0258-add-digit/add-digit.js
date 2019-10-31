23b1a2b876f8cb477c00304c31ecbf6176d6696f
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-31 13:29:20 -0600

    task0258 - add digits: finished

--- task0258-add-digit/add-digit.js
+++ task0258-add-digit/add-digit.js
@@ -2,8 +2,26 @@
  * @param {number}    num   the origin number
  * @return {number}         the added-up digit
  */
-const addDigits = function(num) {
+const addDigits0 = function(num) {
+  while (num >= 10) {
+    let re = 0;
+    while (num > 0) {
+      re += num % 10;
+      num = Math.floor(num / 10);
+    }
+    num = re;
+  }
+  return num;
+};
 
+
+/**
+ * @see https://en.wikipedia.org/wiki/Digital_root#Congruence_formula
+ * @param {number}    num   the origin number
+ * @return {number}         the added-up digit
+ */
+const addDigits = function(num) {
+  return num === 0 ? 0 : 1 + (num - 1) % 9;
 };
 
 
@@ -15,6 +33,7 @@ const main = (callback) => {
   console.log('Task 0258 - Add Digits:');
   [
     38,
+    10,
   ].forEach( v => {
     console.log(`  num=${v}, digit=${addDigits(v)}`);
   });
