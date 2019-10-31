c29fea8baaae5679053b4344252bc55e0be0e481
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-31 16:13:39 -0600

    task0263 - ugly number: finished

--- task0263-ugly-num/ugly-num.js
+++ task0263-ugly-num/ugly-num.js
@@ -2,8 +2,39 @@
  * @param {number}    num   the number to check
  * @return {boolean}        whether an ugly number or not
  */
-const isUgly = function(num) {
+const isUgly0 = function(num) {
+  if (num <= 0) {
+    return false;
+  } else if (num === 1) {
+    return true;
+  }
+  const primes = [2, 3, 5];
+  for (let p of primes) {
+    let re = num / p;
+    while (re === Math.floor(re)) {
+      if (re === 1) {
+        return true;
+      }
+      num = re;
+      re = num / p;
+    }
+  }
+  return false;
+};
 
+
+/**
+ * @param {number}    num   the number to check
+ * @return {boolean}        whether an ugly number or not
+ */
+const isUgly = function(num) {
+  const primes = [2, 3, 5];
+  for (let p of primes) {
+    while (num && num % p == 0) {
+      num /= p;
+    }
+  }
+  return num === 1;
 };
 
 
@@ -14,6 +45,7 @@ const isUgly = function(num) {
 const main = (callback) => {
   console.log('Task 0263 - Ugly Number:');
   [
+    1,
     6,
     8,
     14,
