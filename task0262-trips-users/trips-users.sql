4a079837ec66fbd3a0c665f74bb450630e070d06
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-10-31 15:49:58 -0600

    task0262 - <SQL> trips and users: finished

--- task0262-trips-users/trips-users.sql
+++ task0262-trips-users/trips-users.sql
@@ -1,1 +1,16 @@
 -- Task 0262 - <SQL> Trips and Users
+SELECT
+  t.Request_at    AS [Day],
+  CAST
+  (
+    ROUND(SUM(IIF(t.[Status] <> 'completed', 1.0, 0.0)) / COUNT(*), 2)
+    AS NUMERIC(10, 2)
+  )               AS [Cancellation Rate]
+FROM Trips AS t WITH (NOLOCK)
+INNER JOIN Users AS c WITH (NOLOCK) ON c.Users_Id = t.Client_Id AND c.Banned = 'No'
+INNER JOIN Users AS d WITH (NOLOCK) ON d.Users_Id = t.Driver_Id AND d.Banned = 'No'
+WHERE
+  Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY
+  Request_at
+;
