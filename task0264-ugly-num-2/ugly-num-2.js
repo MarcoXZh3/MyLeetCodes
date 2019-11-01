01ab9597c4c1339b079c2d737bd9c8c42edbe1b7
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-01 16:02:55 -0600

    task0264 - ugly number 2: finished

--- task0264-ugly-num-2/ugly-num-2.js
+++ task0264-ugly-num-2/ugly-num-2.js
@@ -3,7 +3,24 @@
  * @return {number}           the target ugly number
  */
 const nthUglyNumber = function(n) {
-
+  let i2 = 0;
+  let i3 = 0;
+  let i5 = 0;
+  const dp = Array(n).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i < n; i++) {
+    dp[i] = Math.min(dp[i2] * 2, dp[i3] * 3, dp[i5] * 5);
+    if (dp[i] === dp[i2] * 2) {
+      i2 ++;
+    }
+    if (dp[i] === dp[i3] * 3) {
+      i3 ++;
+    }
+    if (dp[i] === dp[i5] * 5) {
+      i5 ++;
+    }
+  }
+  return dp[n - 1];
 };
 
 
@@ -15,6 +32,7 @@ const main = (callback) => {
   console.log('Task 0264 - Ugly Number II:');
   [
     10,
+    1352,
   ].forEach( v => {
     console.log(`  n=${v}, ugly=${nthUglyNumber(v)}`);
   });
