83d61765b5c5b58a502b107e1d87abe8212f829b
MarcoXZh3 <marcoxzh3@gmail.com>
2019-11-02 17:29:52 -0600

    task0268 - missing number: finished

--- task0268-missing-num/missing-num.js
+++ task0268-missing-num/missing-num.js
@@ -1,9 +1,63 @@
 /**
+ * sort and then search
+ * @param {number[]}  nums    the number array
+ * @return {number}           the missing number
+ */
+const missingNumber0 = function(nums) {
+  nums = nums.sort( (a,b)=>a-b );
+  for (let i = 0; i <= nums.length; i++) {
+    if (i !== nums[i]) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+
+/**
+ * hash set
+ * @param {number[]}  nums    the number array
+ * @return {number}           the missing number
+ */
+const missingNumber1 = function(nums) {
+  const map = new Set();
+  for (let num of nums) {
+    map.add(num);
+  }
+  for (let i = 0; i <= nums.length; i++) {
+    if (!map.has(i)) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+
+/**
+ * bit manipulations
+ * @param {number[]}  nums    the number array
+ * @return {number}           the missing number
+ */
+const missingNumber2 = function(nums) {
+  let missing = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+      missing ^= i ^ nums[i];
+  }
+  return missing;
+};
+
+
+/**
+ * by sum of the array
  * @param {number[]}  nums    the number array
  * @return {number}           the missing number
  */
 const missingNumber = function(nums) {
-
+  let sum = nums.length * (nums.length + 1) / 2;
+  for (let num of nums) {
+    sum -= num;
+  }
+  return sum;
 };
 
 
