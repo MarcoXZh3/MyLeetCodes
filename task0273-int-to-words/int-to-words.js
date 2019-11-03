3850ba08f52e7c1124a01cd8e46f65dbc8e04b36
MarcoXZh3 <marcoxzh3@gmail.com>
2019-11-02 23:29:25 -0600

    task0273 - read integer: finished

--- task0273-int-to-words/int-to-words.js
+++ task0273-int-to-words/int-to-words.js
@@ -3,7 +3,43 @@
  * @return {string}           the reading of the number
  */
 const numberToWords = function(num) {
+  const ONES = [
+    '', 'One', 'Two', 'Three', 'Four',
+    'Five', 'Six', 'Seven', 'Eight', 'Nine',
+    'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen',
+    'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen',
+  ];
+  const TENS = [
+    '', 'TEN', 'Twenty', 'Thirty', 'Forty',
+    'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety',
+  ];
+  const THOUS = ['', 'Thousand', 'Million', 'Billion'];
 
+  const read = (num) => {
+    if (num === 0) {
+      return '';
+    } else if (num < 20) {
+      return `${ONES[num]} `;
+    } else if (num < 100) {
+      return `${TENS[Math.floor(num / 10)]} ${read(num % 10)}`;
+    } else {
+      return `${ONES[Math.floor(num / 100)]} Hundred ${read(num % 100)}`;
+    }
+  };
+
+  if (num === 0) {
+    return 'Zero';
+  }
+  let word = '';
+  let i = 0;
+  while (num > 0) {
+    if (num % 1000) {
+      word = `${read(num % 1000)}${THOUS[i]} ${word}`;
+    }
+    num = Math.floor(num / 1000);
+    i ++;
+  }
+  return word.trim();
 };
 
 
