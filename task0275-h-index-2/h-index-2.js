fa101ac2ef4c9c7c94cba8a32d994907de622b9d
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-04 14:27:22 -0700

    task0275 - h index 2: finished

--- task0275-h-index-2/h-index-2.js
+++ task0275-h-index-2/h-index-2.js
@@ -1,9 +1,38 @@
 /**
+ * linear search
+ * @param {number[]}    citations     the number of citations, sorted
+ * @return {number}                   the h index
+ */
+const hIndex0 = function(citations) {
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= citations.length - i) {
+      return citations.length - i;
+    }
+  }
+  return 0;
+};
+
+
+/**
+ * binary search
  * @param {number[]}    citations     the number of citations, sorted
  * @return {number}                   the h index
  */
 const hIndex = function(citations) {
-
+  const n = citations.length;
+  let l = 0;
+  let r = n - 1;
+  while (l <= r) {
+    const m = Math.floor((l + r) / 2);
+    if (citations[m] === n - m) {
+      return n - m;
+    } else if (citations[m] < n - m) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  return n - l;
 };
 
 
