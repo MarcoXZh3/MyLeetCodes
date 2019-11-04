bb66d20d1ed34e8c95e566abcd568495a64a4f33
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-04 13:47:46 -0700

    task0274 - h index: finished

--- task0274-h-index/h-index.js
+++ task0274-h-index/h-index.js
@@ -1,9 +1,38 @@
 /**
+ * sort and then find
+ * @param {number[]}    citations     the number of citations
+ * @return {number}                   the h index
+ */
+const hIndex0 = function(citations) {
+  const sorted = citations.slice().sort( (a,b)=>a-b );
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] >= sorted.length - i) {
+      return sorted.length - i;
+    }
+  }
+  return 0;
+};
+
+
+/**
+ * bucket sorting
  * @param {number[]}    citations     the number of citations
  * @return {number}                   the h index
  */
 const hIndex = function(citations) {
-
+  const n = citations.length;
+  const buckets = Array(n + 1).fill(0);
+  for (let c of citations) {
+    buckets[c > n ? n : c] ++;
+  }
+  let total = 0;
+  for (let i = n; i >= 0; i--) {
+    total += buckets[i];
+    if (total >= i) {
+      return i;
+    }
+  }
+  return 0;
 };
 
 
