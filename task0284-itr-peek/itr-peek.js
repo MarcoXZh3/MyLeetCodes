be84d2e5e0bcdd18d4a27924f3dbb27f126bb3c7
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-06 12:31:45 -0700

    task0284 - peeking iterator: finished

--- task0284-itr-peek/itr-peek.js
+++ task0284-itr-peek/itr-peek.js
@@ -1,33 +1,40 @@
 /* Java Iterator interface reference:
    https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
+    private Iterator<Integer> iterator;
+    private Integer next;
 
-    // initialize any member here.
     public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+        this.next = this.iterator.hasNext() ? this.iterator.next() : null;
     }
 
     // Returns the next element in the iteration without advancing the iterator.
     public Integer peek() {
+        return this.next;
     }
 
     // hasNext() and next() should behave the same as in the Iterator interface.
     // Override them if needed.
     @Override
     public Integer next() {
+        Integer next = this.next;
+        this.next = this.iterator.hasNext() ? this.iterator.next() : null;
+        return next;
     }
 
     @Override
     public boolean hasNext() {
+        return this.next != null;
     }
-}
-*/
+}*/
 
 /**
  * Iterator
  * @param {Array}   arr   the array to iterate
  */
 const Iterator = function(arr) {
-  let idx = 0;
+  let idx = -1;
   arr = arr || [];
   /**
    * iterate to and return the next element
@@ -51,22 +58,22 @@ const Iterator = function(arr) {
  * @param {Iterator}  iterator    the basic iterator to inherit
  */
 const PeekingIterator = function(iterator) {
-
+  let next = iterator.hasNext() ? iterator.next() : null;
   /**
    * peek the next element without iterating to it
    * @returns {object}    the next element, or null if not available
    */
   this.peek = () => {
-
+    return next;
   };
-
-
   /**
    * iterate to and return the next element
    * @returns {object}    the next element, or null if not available
    */
   this.next = () => {
-
+    const re = next;
+    next = iterator.hasNext() ? iterator.next() : null;
+    return re;
   };
 
 
@@ -75,7 +82,7 @@ const PeekingIterator = function(iterator) {
    * @returns {boolean}   whether next element is available or not
    */
   this.hasNext = () => {
-
+    return iterator.hasNext();
   };
 };
 
