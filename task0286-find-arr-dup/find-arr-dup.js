e66232eb1b04875334f13b920e57412caab1af57
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-06 10:14:22 -0700

    task0286 - find duplicate in array: finished

--- task0286-find-arr-dup/find-arr-dup.js
+++ task0286-find-arr-dup/find-arr-dup.js
@@ -1,9 +1,57 @@
 /**
+ * one to one compare
+ * @param {number[]}    nums    the number array
+ * @return {number}             the only duplicate in the array
+ */
+const findDuplicate0 = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return nums[i];
+      }
+    }
+  }
+  return -1;
+};
+
+
+/**
+ * hash set
+ * @param {number[]}    nums    the number array
+ * @return {number}             the only duplicate in the array
+ */
+const findDuplicate1 = function(nums) {
+  const set = new Set();
+  for (let num of nums) {
+    if (set.has(num)) {
+      return num;
+    }
+    set.add(num);
+  }
+  return -1;
+};
+
+
+/**
+ * Floyd's Tortoise and Hare (Cycle Detection)
+ * @see Task0142: Linked List Cycle II
  * @param {number[]}    nums    the number array
  * @return {number}             the only duplicate in the array
  */
 const findDuplicate = function(nums) {
-
+  let tortoise = nums[0];
+  let hare = nums[0];
+  do {
+    tortoise = nums[tortoise];
+    hare = nums[nums[hare]];
+  } while (tortoise !== hare);
+  let i = nums[0];
+  let j = tortoise;
+  while (i !== j) {
+    i = nums[i];
+    j = nums[j];
+  }
+  return i;
 };
 
 
