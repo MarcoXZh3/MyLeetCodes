03363b918940a2dba8c090a7fc1a4bc0bc422b83
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-07 11:42:56 -0700

    task0290 - word pattern: finished

--- task0290-word-pattern/word-pattern.js
+++ task0290-word-pattern/word-pattern.js
@@ -4,7 +4,26 @@
  * @return {boolean}              whether match or not
  */
 const wordPattern = function(pattern, str) {
-
+  const dict1 = {};
+  const dict2 = {};
+  str = str.split(' ');
+  if (str.length !== pattern.length) {
+    return false;
+  }
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern.charAt(i);
+    const sub = str[i];
+    if (!dict1[char]) {
+      if (dict2[sub]) {
+        return false;
+      }
+      dict1[char] = sub;
+      dict2[sub] = char;
+    } else if (dict1[char] !== sub  || dict2[sub] !== char) {
+      return false;
+    }
+  }
+  return true;
 };
 
 
@@ -19,6 +38,7 @@ const main = (callback) => {
     ['abba', 'dog cat cat fish'],
     ['aaaa', 'dog cat cat dog'],
     ['abba', 'dog dog dog dog'],
+    ['abc', 'b c a'],
   ].forEach( vs => {
     console.log(`  match=${wordPattern(vs[0], vs[1])}, pattern="${vs[0]}", str="${vs[1]}"`);
   });
