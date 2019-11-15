e0486b04f3e1b0a387b5aee58bda2e8b3c8ffdf4
MarcoXZh3 <marcoxzh3@gmail.com>
2019-11-14 19:08:17 -0700

    task0304 - immutable 2d array range sum: finished

--- task0304-range-sum-2d/range-sum-2d.js
+++ task0304-range-sum-2d/range-sum-2d.js
@@ -1,7 +1,7 @@
 /**
  * @param {number[][]}    matrix    the 2d number array
  */
-const NumMatrix = function(matrix) {
+const NumMatrix0 = function(matrix) {
   /** 
    * @param {number}      row1      index of 1st row
    * @param {number}      col1      index of 1st column
@@ -10,7 +10,41 @@ const NumMatrix = function(matrix) {
    * @return {number}               the summation between the given area
    */
   this.sumRegion = (row1, col1, row2, col2) => {
+    let sum = 0;
+    for (let i = row1; i <= row2; i++) {
+      for (let j = col1; j <= col2; j++) {
+        sum += matrix[i][j];
+      }
+    }
+    return sum;
+  };
+};
 
+
+/**
+ * @param {number[][]}    matrix    the 2d number array
+ */
+const NumMatrix = function(matrix) {
+  const m = matrix ? matrix.length : 0;
+  const n = m > 0 ? matrix[0].length : 0;
+  const cache = [...Array(m+1).keys()].map( _=>[...Array(n+1).keys()].map( _=>0 ) );
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      cache[i+1][j+1] = cache[i+1][j] + cache[i][j+1] + matrix[i][j] - cache[i][j];
+    }
+  }
+
+
+  /** 
+   * @param {number}      row1      index of 1st row
+   * @param {number}      col1      index of 1st column
+   * @param {number}      row2      index of 2nd row
+   * @param {number}      col2      index of 2nd column
+   * @return {number}               the summation between the given area
+   */
+  this.sumRegion = (row1, col1, row2, col2) => {
+    return cache[row2+1][col2+1] + cache[row1][col1]
+         - cache[row1][col2+1] - cache[row2+1][col1];
   };
 };
 
