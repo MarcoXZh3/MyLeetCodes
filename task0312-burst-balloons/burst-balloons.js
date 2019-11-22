ce658e9b07acafb6b2fc802ab97a4e0dc25aa8ec
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-11-21 17:06:46 -0700

    task0312 - burst balloons: finished

--- task0312-burst-balloons/burst-balloons.js
+++ task0312-burst-balloons/burst-balloons.js
@@ -3,7 +3,21 @@
  * @return {number}             max number of coin
  */
 const maxCoins = function(nums) {
-
+  nums = [1, ...nums, 1];
+  const n = nums.length;
+  const dp = [...Array(n).keys()].map( _=>[...Array(n).keys()].map( _=>0 ));
+  for (let k = 2; k < n; k++) {
+    for (let left = 0; left < n - k; left++) {
+      let right = left + k;
+      for (let i = left + 1; i < right; i++) {
+        dp[left][right] = Math.max(
+          dp[left][right],
+          nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]
+        );
+      }
+    }
+  }
+  return dp[0][n - 1];
 };
 
 
