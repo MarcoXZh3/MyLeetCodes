5439bcf40e0e552e85711833accf0114ecba6273
MarcoXZh3 <marcoxzh3@gmail.com>
2019-11-28 19:33:00 -0700

    task0315 - smaller number count after self: finished

--- task0315-smaller-num-cnt/smaller-num-cnt.js
+++ task0315-smaller-num-cnt/smaller-num-cnt.js
@@ -3,7 +3,36 @@
  * @return {number[]}           the count array
  */
 const countSmaller = function(nums) {
+  const Node = function(val, sum) {
+    this.val = val;
+    this.sum = sum;
+    this.dup = 1;
+    this.left = null;
+    this.right = null;
+  };
 
+  const insert = (num, node, i, pre) => {
+    if (!node) {
+      node = new Node(num, 0);
+      res[i] = pre;
+    } else if (node.val === num) {
+      node.dup ++;
+      res[i] = pre + node.sum;
+    } else if (node.val > num) {
+      node.sum ++;
+      node.left = insert(num, node.left, i, pre);
+    } else {
+      node.right = insert(num, node.right, i, pre + node.dup + node.sum);
+    }
+    return node;
+  };
+
+  const res = Array(nums.length).fill(0);
+  let root = null;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    root = insert(nums[i], root, i, 0);
+  }
+  return res;
 };
 
 
