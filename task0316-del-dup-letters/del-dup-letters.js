e6c8a1fca2ce66a8c1889badfe407f1c913cb20a
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-02 13:30:29 -0700

    task0316 - removed duplicated letters: finished

--- task0316-del-dup-letters/del-dup-letters.js
+++ task0316-del-dup-letters/del-dup-letters.js
@@ -3,7 +3,24 @@
  * @return {string}         the string with duplicated letters removed
  */
 const removeDuplicateLetters = function(s) {
-
+  const recursion = (str) => {
+    const cnt = Array(26).fill(0);
+    let pos = 0;
+    for (let i = 0; i < str.length; i++) {
+      cnt[str.charCodeAt(i) - 'a'.charCodeAt(0)] ++;
+    }
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) < str.charAt(pos)) {
+        pos = i;
+      }
+      if (-- cnt[str.charCodeAt(i) - 'a'.charCodeAt(0)] === 0) {
+        break ;
+      }
+    }
+    const sub = str.substring(pos + 1).split(str.charAt(pos)).join('');
+    return str.length === 0 ? '' : str.charAt(pos) + recursion(sub);
+  };
+  return recursion(s);
 };
 
 
