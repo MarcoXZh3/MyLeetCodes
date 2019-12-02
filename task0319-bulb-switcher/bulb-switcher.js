d2749a9e82fafaf52a29ce77fae94a567c1f051f
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-02 14:29:23 -0700

    task0319 - bulb switcher: finished

--- task0319-bulb-switcher/bulb-switcher.js
+++ task0319-bulb-switcher/bulb-switcher.js
@@ -3,7 +3,7 @@
  * @return {number}           how many bulbs are on after all rounds
  */
 const bulbSwitch = function(n) {
-
+  return Math.floor(Math.sqrt(n));
 };
 
 
@@ -14,7 +14,8 @@ const bulbSwitch = function(n) {
 const main = (callback) => {
   console.log('Task 0319 - Bulb Switcher:');
   [
-    3
+    3,
+    99999999,
   ].forEach( v => {
     console.log(`  n=${v}; on=${bulbSwitch(v)}`);
   });
