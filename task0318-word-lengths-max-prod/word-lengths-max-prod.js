fd3a4043a77bf425ea1790d9dcba5d1c0561b574
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-02 14:04:01 -0700

    task0318 - max product of word lengths: finished

--- task0318-word-lengths-max-prod/word-lengths-max-prod.js
+++ task0318-word-lengths-max-prod/word-lengths-max-prod.js
@@ -1,9 +1,25 @@
 /**
+ * use binaries to represent words and bit wise and to check duplicated letters
  * @param {string[]}    words     the word list
  * @return {number}               the maximum product
  */
 const maxProduct = function(words) {
+  const masks = Array(words.length).fill(0);
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      masks[i] |= 1 << (words[i].charCodeAt(j) - 'a'.charCodeAt(0));
+    }
+  }
 
+  let max = 0;
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if ((masks[i] & masks[j]) === 0) {
+        max = Math.max(max, words[i].length * words[j].length);
+      }
+    }
+  }
+  return max;
 };
 
 
