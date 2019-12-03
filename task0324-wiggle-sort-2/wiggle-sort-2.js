8157e2b71d6a38b4ecc2c349c2ab8880de68effc
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-03 13:35:13 -0700

    task0324 - wiggle sort 2: finished

--- task0324-wiggle-sort-2/wiggle-sort-2.js
+++ task0324-wiggle-sort-2/wiggle-sort-2.js
@@ -2,8 +2,48 @@
  * @param {number[]}    nums    the number array
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const wiggleSort = function(nums) {
+const wiggleSort0 = function(nums) {
+  const n = nums.length;
+  const m = (n + 1) >> 1;
+  const copy = nums.slice().sort( (a,b)=>a-b );
+  for (let i = m - 1, j = 0; i >= 0; i--, j += 2) {
+    nums[j] = copy[i];
+  }
+  for (let i = n - 1, j = 1; i >= m; i--, j += 2) {
+    nums[j] = copy[i];
+  }
+};
 
+
+/**
+ * @param {number[]}    nums    the number array
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const wiggleSort = function(nums) {
+  const get = (i) => (2 * i + 1) % (n | 1);
+  const swap = (i, j) => {
+    const tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  };
+  const kthSmallest = function(arr, k) {
+    return k < arr.length ? arr.slice().sort( (a,b)=>a-b )[k] : null;
+  };
+
+  const n = nums.length;
+  const m = kthSmallest(nums, (n + 1) >> 1);
+  let i = 0;
+  let j = 0;
+  let k = n - 1;
+  while (j <= k) {
+    if (nums[get(j)] > m) {
+      swap(get(i ++), get(j ++));
+    } else if (nums[get(j)] < m) {
+      swap(get(j), get(k --));
+    } else {
+      j ++;
+    }
+  }
 };
 
 
