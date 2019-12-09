6e4bbbc18ba3231835aab2e7d3c9a0e6f46c1f12
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-09 11:25:41 -0700

    task0332 - reconstruct itinerary: finished

--- task0332-reconstruct-itinerary/reconstruct-itinerary.js
+++ task0332-reconstruct-itinerary/reconstruct-itinerary.js
@@ -1,9 +1,59 @@
 /**
+ * DFS by recursion
+ * @param {string[][]}    tickets   the ticket list
+ * @return {string[]}               the airport list in order
+ */
+const findItinerary0 = function(tickets) {
+  const dfs = (cur) => {
+    while (map[cur] && map[cur].length > 0) {
+      dfs(map[cur].shift());
+    }
+    res.splice(0, 0, cur);
+  };
+
+  const map = {};
+  for (let t of tickets) {
+    let arr = map[t[0]] || [];
+    let i = 0;
+    while (i < arr.length && t[1] > arr[i]) {
+      i ++;
+    }
+    arr.splice(i, 0, t[1]);
+    map[t[0]] = arr;
+  }
+
+  const res = [];
+  dfs('JFK');
+  return res;
+};
+
+
+/**
+ * DFS by stack
  * @param {string[][]}    tickets   the ticket list
  * @return {string[]}               the airport list in order
  */
 const findItinerary = function(tickets) {
+  const map = {};
+  for (let t of tickets) {
+    let arr = map[t[0]] || [];
+    let i = 0;
+    while (i < arr.length && t[1] > arr[i]) {
+      i ++;
+    }
+    arr.splice(i, 0, t[1]);
+    map[t[0]] = arr;
+  }
 
+  const res = [];
+  const stack = ['JFK'];
+  while (stack.length > 0) {
+    while (map[stack[stack.length - 1]] && map[stack[stack.length - 1]].length > 0) {
+      stack.push(map[stack[stack.length - 1]].shift());
+    }
+    res.splice(0, 0, stack.pop());
+  }
+  return res;
 };
 
 
@@ -16,6 +66,7 @@ const main = (callback) => {
   [
     [ ['MUC', 'LHR'], ['JFK', 'MUC'], ['SFO', 'SJC'], ['LHR', 'SFO'] ],
     [ ['JFK', 'SFO'], ['JFK', 'ATL'], ['SFO', 'ATL'], ['ATL','JFK'], ['ATL','SFO'] ],
+    [ ['JFK', 'KUL'], ['JFK', 'NRT'], ['NRT','JFK'] ],
   ].forEach( vs => {
     console.log(`  tickets=${JSON.stringify(vs)}`);
     console.log(`  itinerary=${JSON.stringify(findItinerary(vs))}`);
