c3568e4453bdf2d47a76e91836424fca1f2257b4
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-10 16:57:14 -0700

    task0344 - reverse string: finished

--- task0344-reverse-string/reverse-string.js
+++ task0344-reverse-string/reverse-string.js
@@ -3,7 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 const reverseString = function(s) {
-
+  for (let i = 0; i < s.length / 2; i++) {
+    let tmp = s[i];
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = tmp;
+  }
 };
 
 
