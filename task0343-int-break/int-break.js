78c1a27945abd71854ec252af03a0a6d91a602ef
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-10 16:47:45 -0700

    task0343 - integer break: finished

--- task0343-int-break/int-break.js
+++ task0343-int-break/int-break.js
@@ -2,8 +2,56 @@
  * @param {number}    n   the integer to break
  * @return {number}       the maximized product
  */
-const integerBreak = function(n) {
+const integerBreak0 = function(n) {
+  const dp = Array(n + 1).fill(0);
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j < i; j++) {
+      dp[i] = Math.max(
+        dp[i],
+        Math.max(j, dp[j]) * Math.max(i - j, dp[i - j])
+      );
+    }
+  }
+  return dp[n];
+};
 
+
+/**
+ * @param {number}    n   the integer to break
+ * @return {number}       the maximized product
+ */
+const integerBreak1 = function(n) {
+  if (n === 2) {
+    return 1;
+  } else if (n === 3) {
+    return 2;
+  }
+  let prod = 1;
+  while (n > 4) {
+    prod *= 3;
+    n -= 3;
+  }
+  return prod * n;
+};
+
+
+/**
+ * @param {number}    n   the integer to break
+ * @return {number}       the maximized product
+ */
+const integerBreak = function(n) {
+  if (n === 2) {
+    return 1;
+  } else if (n === 3) {
+    return 2;
+  } else if (n % 3 === 0) {
+    return Math.pow(3, Math.floor(n / 3));
+  } else if (n % 3 === 1) {
+    return 2 * 2 * Math.pow(3, Math.floor((n - 4) / 3));
+  } else {
+    return 2 * Math.pow(3, Math.floor(n / 3));
+  }
 };
 
 
