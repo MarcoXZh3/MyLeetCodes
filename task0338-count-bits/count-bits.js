2cddf7783b3ca589b86d1fddaff9aea42bfd3805
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-10 14:12:05 -0700

    task0338 - count bits: finished

--- task0338-count-bits/count-bits.js
+++ task0338-count-bits/count-bits.js
@@ -1,9 +1,62 @@
 /**
+ * basic - convert to binary then count 1s
+ * @param {number}      num     the upper boundary number
+ * @return {number[]}           how many 1s in the numbers
+ */
+const countBits0 = function(num) {
+  const res = Array(num + 1).fill(0);
+  for (let i = 0; i <= num; i++) {
+    res[i] = i.toString(2).split('').filter(x=>x==='1').length;
+  }
+  return res;
+};
+
+
+/**
+ * basic - reduce each number and find the number of 1s
+ * @param {number}      num     the upper boundary number
+ * @return {number[]}           how many 1s in the numbers
+ */
+const countBits1 = function(num) {
+  const res = Array(num + 1).fill(0);
+  for (let i = 0; i <= num; i++) {
+    let n = i;
+    while (n > 0) {
+      res[i] += n % 2;
+      n = Math.floor(n / 2);
+    }
+  }
+  return res;
+};
+
+
+/**
+ * @param {number}      num     the upper boundary number
+ * @return {number[]}           how many 1s in the numbers
+ */
+const countBits2 = function(num) {
+  const res = Array(num + 1).fill(0);
+  for (let i = 0; i <= num; i++) {
+    res[i] = res[i >> 1] + (i & 1);
+  }
+  return res;
+};
+
+
+/**
  * @param {number}      num     the upper boundary number
  * @return {number[]}           how many 1s in the numbers
  */
 const countBits = function(num) {
-
+  const res = Array(num + 1).fill(0);
+  let offset = 1;
+  for (let i = 1; i <= num; i++) {
+    if (offset * 2 === i) {
+      offset *= 2;
+    }
+    res[i] = res[i - offset] + 1;
+  }
+  return res;
 };
 
 
