57e0a1b85ba212db3d8bdd5d94e3cca0a8dc6dc4
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-11 12:14:48 -0700

    task0350 - intersection of 2 arrays II: finished

--- task0350-arr-intersection-2/array-intersection-2.js
+++ task0350-arr-intersection-2/array-intersection-2.js
@@ -1,10 +1,51 @@
 /**
+ * hash map
+ * @param {number[]}      nums1     the 1st number list
+ * @param {number[]}      nums2     the 2nd number list
+ * @return {number[]}               the intersection
+ */
+const intersect0 = function(nums1, nums2) {
+  const map = {};
+  const res = [];
+  for (let i = 0; i < nums1.length; i++) {
+    const k = `${nums1[i]}`;
+    map[k] = 1 + (map[k] || 0);
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    const k = `${nums2[i]}`;
+    if (map[k]) {
+      res.push(nums2[i]);
+      map[k] --;
+    }
+  }
+  return res;
+};
+
+
+/**
+ * two pointers
  * @param {number[]}      nums1     the 1st number list
  * @param {number[]}      nums2     the 2nd number list
  * @return {number[]}               the intersection
  */
 const intersect = function(nums1, nums2) {
-
+  nums1 = nums1.sort( (a,b)=>a-b );
+  nums2 = nums2.sort( (a,b)=>a-b );
+  const res = [];
+  let i = 0;
+  let j = 0;
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] > nums2[j]) {
+      j ++;
+    } else if (nums1[i] < nums2[j]) {
+      i ++;
+    } else {
+      res.push(nums1[i]);
+      i ++;
+      j ++;
+    }
+  }
+  return res;
 };
 
 
