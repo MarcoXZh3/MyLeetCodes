8095ae4a8ce5456712d6c09ec37c588875469580
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-10 17:13:27 -0700

    task0345 - reverse vowels of string: finished

--- task0345-reverse-vowels/reverse-vowels.js
+++ task0345-reverse-vowels/reverse-vowels.js
@@ -3,7 +3,22 @@
  * @return {string}           the reversed string
  */
 const reverseVowels = function(s) {
-
+  const VOWELS = 'aeiouAEIOU';
+  const res = s.split('');
+  for (let i = 0, j = res.length - 1; i < j; ) {
+    if (!VOWELS.includes(res[i])) {
+      i ++;
+    } else if (!VOWELS.includes(res[j])) {
+      j --;
+    } else {
+      let tmp = res[i];
+      res[i] = res[j];
+      res[j] = tmp;
+      i ++;
+      j --;
+    }
+  }
+  return res.join('');
 };
 
 
@@ -16,6 +31,7 @@ const main = (callback) => {
   [
     'hello',
     'leetcode',
+    '',
   ].forEach( v => {
     console.log(`  str="${v}", rev="${reverseVowels(v)}"`);
   });
