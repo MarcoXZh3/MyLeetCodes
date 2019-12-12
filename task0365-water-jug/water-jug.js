3331f0dac5f316ec77b4079bee13be9e99cb9f4a
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-12 16:52:47 -0700

    task0365 - water jug problem: finished

--- task0365-water-jug/water-jug.js
+++ task0365-water-jug/water-jug.js
@@ -1,11 +1,51 @@
 /**
+ * Queue based BFS
+ * @param {number}      x     capacity of the 1st jug
+ * @param {number}      y     capacity of the 2nd jug
+ * @param {number}      z     water to get
+ * @return {boolean}          whether solvable or not
+ */
+const canMeasureWater0 = function(x, y, z) {
+  if (z < 0 || z > x + y) {
+    return false;
+  }
+  const set = new Set();
+  const queue = [0];
+  while (queue.length > 0) {
+    const n = queue.shift();
+    if (n + x <= x + y && !set.has(n + x)) {
+      set.add(n + x);
+      queue.push(n + x);
+    }
+    if (n + y <= x + y && !set.has(n + y)) {
+      set.add(n + y);
+      queue.push(n + y);
+    }
+    if (n - x >= 0 && !set.has(n - x)) {
+      set.add(n - x);
+      queue.push(n - x);
+    }
+    if (n - y >= 0 && !set.has(n - y)) {
+      queue.push(n - y);
+    }
+    if (set.has(z)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+/**
+ * GCD
  * @param {number}      x     capacity of the 1st jug
  * @param {number}      y     capacity of the 2nd jug
  * @param {number}      z     water to get
  * @return {boolean}          whether solvable or not
  */
 const canMeasureWater = function(x, y, z) {
-
+  const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+  return z == 0 || z - x <= y && z % gcd(x, y) === 0;
 };
 
 
