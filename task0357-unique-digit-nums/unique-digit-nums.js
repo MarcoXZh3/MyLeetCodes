2a21f3a8b275b9ff900ac723d1c7340a398b5435
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-12 10:58:19 -0700

    task0357 - count numbers with unique digits: finished

--- task0357-unique-digit-nums/unique-digit-nums.js
+++ task0357-unique-digit-nums/unique-digit-nums.js
@@ -1,9 +1,43 @@
 /**
+ * dynamic programming
+ * @param {number}      n       number of digits of the upper bound number
+ * @return {number}             number of target numbers
+ */
+const countNumbersWithUniqueDigits0 = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  const dp = Array(n).fill(0);
+  dp[0] = 10;
+  for (let i = 1; i < n; i++) {
+    let cnt = 9;
+    let re = cnt;
+    for (let j = 1; j <= i; j++) {
+      re *= cnt --;
+    }
+    dp[i] = re + dp[i - 1];
+  }
+  return dp[n - 1];
+};
+
+
+/**
  * @param {number}      n       number of digits of the upper bound number
  * @return {number}             number of target numbers
  */
 const countNumbersWithUniqueDigits = function(n) {
-
+  if (n === 0) {
+    return 1;
+  }
+  let res = 10;
+  let uniqueDigits = 9;
+  let availableNumber = 9;
+  while (n -- > 1 && availableNumber > 0) {
+      uniqueDigits = uniqueDigits * availableNumber;
+      res += uniqueDigits;
+      availableNumber --;
+  }
+  return res;
 };
 
 
