947a69e8cd8aa1fcb276720598407f3a037f23f7
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-13 12:15:43 -0700

    task0368 - largest divisible subset: finished

--- task0368-max-divisible-sub/max-divisible-sub.js
+++ task0368-max-divisible-sub/max-divisible-sub.js
@@ -3,7 +3,29 @@
  * @return {number[]}               a max sub set
  */
 const largestDivisibleSubset = function(nums) {
-
+  nums = nums.sort( (a,b)=>a-b );
+  const cnt = Array(nums.length).fill(1);
+  const pre = Array(nums.length).fill(-1);
+  let max = 0;
+  let idx = -1;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (nums[i] % nums[j] === 0 && cnt[j] + 1 > cnt[i]) {
+        cnt[i] = cnt[j] + 1;
+        pre[i] = j;
+      }
+    }
+    if (cnt[i] > max) {
+      max = cnt[i];
+      idx = i;
+    }
+  }
+  const res = [];
+  while (idx !== -1) {
+    res.unshift(nums[idx]);
+    idx = pre[idx];
+  }
+  return res;
 };
 
 
