1f284041bd25b476b4f655c9d93f868d3ea2ec1a
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-13 12:31:01 -0700

    task0371 - sum of 2 integers: finished

--- task0371-sum-2-int/sum-2-int.js
+++ task0371-sum-2-int/sum-2-int.js
@@ -3,8 +3,28 @@
  * @param {number}    b     the 2nd integer
  * @return {number}         the sum of the two integers
  */
-const getSum = function(a, b) {
+const getSum0 = function(a, b) {
+  return b === 0 ? a : getSum(a ^ b, (a & b) << 1);
+};
 
+
+/**
+ * @param {number}    a     the 1st integer
+ * @param {number}    b     the 2nd integer
+ * @return {number}         the sum of the two integers
+ */
+const getSum = function(a, b) {
+  if (a === 0) {
+    return b;
+  } else if (b === 0) {
+    return a;
+  }
+  while (b !== 0) {
+    let carry = a & b;
+    a = a ^ b;
+    b = carry << 1;
+  }
+  return a;
 };
 
 
