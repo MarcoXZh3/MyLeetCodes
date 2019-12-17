ae8ce4de67bf324ceb27188028729a64f436fd39
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-17 13:15:22 -0700

    task0377 - combination sum 4: finished

--- task0377-comb-sum-4/comb-sum-4.js
+++ task0377-comb-sum-4/comb-sum-4.js
@@ -3,8 +3,43 @@
  * @param {number}      target    the target sum
  * @return {number}               how many combinations
  */
-const combinationSum4 = function(nums, target) {
+const combinationSum4_0 = function(nums, target) {
+  const recursion = (target) => {
+    if (dp[target] != -1) {
+      return dp[target];
+    }
+    let re = 0;
+    for (let n of nums) {
+      if (target >= n) {
+        re += recursion(target - n);
+      }
+    }
+    dp[target] = re;
+    return re;
+  };
 
+  const dp = Array(target + 1).fill(-1);
+  dp[0] = 1;
+  return recursion(target);
+};
+
+
+/**
+ * @param {number[]}    nums      the number array
+ * @param {number}      target    the target sum
+ * @return {number}               how many combinations
+ */
+const combinationSum4 = function(nums, target) {
+  const dp = Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i - nums[j] >= 0) {
+        dp[i] += dp[i - nums[j]];
+      }
+    }
+  }
+  return dp[target];
 };
 
 
