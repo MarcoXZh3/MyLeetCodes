7e51c4a0cf23aceac1d4e576edddb3a0d357e5e8
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-17 14:59:30 -0700

    task0380 - insert delete random in O(1) with dups: finished

--- task0380-o1-add-del-rand-2/o1-add-del-rand-2.js
+++ task0380-o1-add-del-rand-2/o1-add-del-rand-2.js
@@ -2,12 +2,19 @@
  * O(1) collection allowing duplicated elements
  */
 const RandomizedCollection = function() {
+  const vals = [];
+  const idxs = {};
   /**
    * Inserts a value to the set
    * @param {number}      val     the value to insert
    * @return {boolean}            whether already exists before insertion or not
    */
   this.insert = function(val) {
+    const key = `${val}`;
+    idxs[key] = idxs[key] || new Set();
+    idxs[key].add(vals.length);
+    vals.push(val);
+    return idxs[key].size === 1;
   };
 
   /**
@@ -16,6 +23,20 @@ const RandomizedCollection = function() {
    * @return {boolean}            whether already exists before deletion or not
    */
   this.remove = function(val) {
+    let key = `${val}`;
+    if (!idxs[key] || idxs[key].size === 0) {
+      return false;
+    }
+    const idx = idxs[key].values().next().value;
+    idxs[key].delete(idx);
+    const last = vals.pop();
+    if (idx !== vals.length) {
+      vals[idx] = last;
+      key = `${last}`;
+      idxs[key].delete(vals.length);
+      idxs[key].add(idx);
+    }
+    return true;
   };
 
   /**
@@ -23,6 +44,7 @@ const RandomizedCollection = function() {
    * @return {number}             the random value from the set
    */
   this.getRandom = function() {
+    return vals.length === 0 ? -1 : vals[Math.floor(Math.random() * vals.length)];
   };
 };
 
@@ -33,14 +55,23 @@ const RandomizedCollection = function() {
  */
 const main = (callback) => {
   console.log('Task 0381 - Insert Delete GetRandom O(1) - Duplicates allowed:');
-  const collection = new RandomizedCollection();
-  console.log(`  const collection = new RandomizedCollection();`);
-  console.log(`  collection.insert(1);     // ${collection.insert(1)}`);
-  console.log(`  collection.insert(1);     // ${collection.insert(1)}`);
-  console.log(`  collection.insert(2);     // ${collection.insert(2)}`);
-  console.log(`  collection.getRandom();   // ${collection.getRandom()}`);
-  console.log(`  collection.remove(1);     // ${collection.remove(1)}`);
-  console.log(`  collection.getRandom();   // ${collection.getRandom()}`);
+  const collection1 = new RandomizedCollection();
+  console.log(`  const collection1 = new RandomizedCollection();`);
+  console.log(`  collection1.insert(1);     // ${collection1.insert(1)}`);
+  console.log(`  collection1.insert(1);     // ${collection1.insert(1)}`);
+  console.log(`  collection1.insert(2);     // ${collection1.insert(2)}`);
+  console.log(`  collection1.getRandom();   // ${collection1.getRandom()}`);
+  console.log(`  collection1.remove(1);     // ${collection1.remove(1)}`);
+  console.log(`  collection1.getRandom();   // ${collection1.getRandom()}`);
+  const collection2 = new RandomizedCollection();
+  console.log(`  const collection2 = new RandomizedCollection();`);
+  console.log(`  collection2.insert(0);     // ${collection2.insert(0)}`);
+  console.log(`  collection2.remove(0);     // ${collection2.remove(0)}`);
+  console.log(`  collection2.insert(1);     // ${collection2.insert(1)}`);
+  console.log(`  collection2.remove(0);     // ${collection2.remove(0)}`);
+  console.log(`  collection2.getRandom();   // ${collection2.getRandom()}`);
+  console.log(`  collection2.insert(2);     // ${collection2.insert(2)}`);
+  console.log(`  collection2.getRandom();   // ${collection2.getRandom()}`);
   if (callback) {
     callback();
   }
