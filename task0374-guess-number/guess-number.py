f966dfa77408066da431d77e4664d30ec3f78bbe
MarcoXZh3 <marcoxzh3@gmail.com>
2019-12-17 21:41:28 -0700

    task0374 - guess number: finished

--- task0374-guess-number/guess-number.py
+++ task0374-guess-number/guess-number.py
@@ -12,8 +12,41 @@ class Solution:
             return 0
 
 
+    def guessNumber0(self, n: int) -> int:
+        low = 1
+        high = n
+        while low <= high:
+            mid = (high + low) // 2
+            res = self.guess(mid)
+            if res == 0:
+                return mid
+            elif res < 0:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+
     def guessNumber(self, n: int) -> int:
-        pass
+        low = 1
+        high = n
+        while low <= high:
+            mid1 = low + (high - low) // 3
+            mid2 = high - (high - low) // 3
+            res1 = self.guess(mid1)
+            res2 = self.guess(mid2)
+            if res1 == 0:
+                return mid1
+            if res2 == 0:
+                return mid2
+            elif res1 < 0:
+                high = mid1 - 1
+            elif res2 > 0:
+                low = mid2 + 1
+            else:
+                low = mid1 + 1
+                high = mid2 - 1
+        return -1
 
 
 if __name__ == '__main__':
