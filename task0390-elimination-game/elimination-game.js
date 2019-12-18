a48bbc755d601874944625a3cdd68ec47a2c5ee6
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-18 16:21:00 -0700

    task0390 - elimination game: finished

--- task0390-elimination-game/elimination-game.js
+++ task0390-elimination-game/elimination-game.js
@@ -2,8 +2,30 @@
  * @param {number}      n     the max number
  * @return {number}           the last number remaining
  */
-const lastRemaining = function(n) {
+const lastRemaining0 = function(n) {
+  let left = true;
+  let remaining = n;
+  let step = 1;
+  let head = 1;
+  while (remaining > 1) {
+    if (left || remaining % 2 === 1) {
+      head += step;
+    }
+    remaining = Math.floor(remaining / 2);
+    step *= 2;
+    left = !left;
+  }
+  return head;
+};
 
+
+/**
+ * @param {number}      n     the max number
+ * @return {number}           the last number remaining
+ */
+const lastRemaining = function(n) {
+  const recursion = (n) => n === 1 ? 1 : 2 * ((n >> 1) + 1 - recursion(n>>1));
+  return recursion(n);
 };
 
 
@@ -15,6 +37,7 @@ const main = (callback) => {
   console.log('Task 0390 - Elimination Game:');
   [
     9,
+    100000000,
   ].forEach( v => {
     console.log(`  n=${v}, last=${lastRemaining(v)}`);
   });
