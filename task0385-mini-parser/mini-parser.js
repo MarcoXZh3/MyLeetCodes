883250f9abacf52fdb991bcbccd411be6607575b
MarcoXZh3 <marcoxzh3@gmail.com>
2019-12-17 20:14:40 -0700

    task0385 - nested integer parser: finished

--- task0385-mini-parser/mini-parser.js
+++ task0385-mini-parser/mini-parser.js
@@ -3,12 +3,14 @@
  * @param {object}      val         value for the nested integer
  */
 const NestedInteger = function(val) {
+  let isInt = true;
+  let data;
   /**
    * Return true if this NestedInteger holds a single integer, rather than a nested list.
    * @returns {boolean}             whether an integer or not
    */
   this.isInteger = () => {
-    return !Array.isArray(val);
+    return isInt;
   };
   /**
    * Return the single integer that this NestedInteger holds, if it holds a single integer
@@ -16,7 +18,27 @@ const NestedInteger = function(val) {
    * @returns {integer}             the integer or null
    */
   this.getInteger = () => {
-    return Array.isArray(val) ? null : val;
+    return isInt ? data : null;
+  };
+  /**
+   * Set this NestedInteger to hold a single integer equal to value.
+   * @param {number}    val       value for the nested integer
+   */
+  this.setInteger = (val) => {
+    isInt = true;
+    data = val;
+  };
+  /**
+   * Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+   * @param {number}    val       value for the nested integer
+   */
+  this.add = (val) => {
+    if (isInt) {
+      isInt = false;
+      data = [val];
+    } else {
+      data.push(val);
+    }
   };
   /**
    * Return the nested list that this NestedInteger holds, if it holds a nested list
@@ -24,7 +46,7 @@ const NestedInteger = function(val) {
    * @returns {NestedInteger[]}     the nested integer list
    */
   this.getList = () => {
-    return Array.isArray(val) ? val : null;
+    return isInt ? null : data;
   };
   /**
    * convert the object to a string
@@ -34,6 +56,14 @@ const NestedInteger = function(val) {
     return this.isInteger() ? `${this.getInteger()}` :
            `[${this.getList().map(e=>e.toString()).join(',')}]`;
   }
+
+  // initialization
+  if (Array.isArray(val)) {
+    isInt = false;
+    data = val.slice();
+  } else if (Number.isInteger(val)) {
+    this.setInteger(val);
+  }
 };
 
 
@@ -42,7 +72,31 @@ const NestedInteger = function(val) {
  * @return {NestedInteger}      the nested integer instance
  */
 const deserialize = function(s) {
-  return new NestedInteger(0);
+  const recursion = (s) => {
+    const re = new NestedInteger();
+    if (!s || s.length === 0) {
+      return re;
+    }
+    if (s.charAt(0) !== '[') {
+      re.setInteger(parseInt(s, 10));
+    } else if (s.length > 2) {
+      let start = 1;
+      let cnt = 0;
+      for (let i = 1; i < s.length; i++) {
+        const char = s.charAt(i);
+        if (cnt === 0 && (char === ',' || i === s.length - 1)) {
+          re.add(recursion(s.substring(start, i)));
+          start = i + 1;
+        } else if (char === '[') {
+          cnt ++;
+        } else if (char === ']') {
+          cnt --;
+        }
+      }
+    }
+    return re;
+  };
+  return recursion(s);
 };
 
 
