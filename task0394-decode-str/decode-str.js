e96de1fe9a48686e1b2419bbbca91617f4e66076
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-19 14:57:32 -0700

    task0394 - decode string: finished

--- task0394-decode-str/decode-str.js
+++ task0394-decode-str/decode-str.js
@@ -2,8 +2,66 @@
  * @param {string}    s     the encoded string
  * @return {string}         the decoded string
  */
-const decodeString = function(s) {
+const decodeString0 = function(s) {
+  let re = '';
+  const stack1 = [];
+  const stack2 = [];
+  let idx = 0;
+  while (idx < s.length) {
+    const char = s.charAt(idx);
+    if (/[0-9]/.test(char)) {
+      let cnt = 0;
+      while (/[0-9]/.test(s.charAt(idx))) {
+        cnt = cnt * 10 + parseInt(s.charAt(idx ++), 10);
+      }
+      stack1.push(cnt);
+      idx --;
+    } else if (char === '[') {
+      stack2.push(re);
+      re = '';
+    } else if (char === ']') {
+      let cur = stack2.pop();
+      let cnt = stack1.pop();
+      while (cnt -- > 0) {
+        cur += re;
+      }
+      re = cur;
+    } else {
+      re += char;
+    }
+    idx ++;
+  }
+  return re;
+};
 
+
+/**
+ * @param {string}    s     the encoded string
+ * @return {string}         the decoded string
+ */
+const decodeString = function(s) {
+  const recursion = () => {
+    let re = '';
+    while (idx < s.length && s.charAt(idx) !== ']') {
+      if (!/[0-9]/.test(s.charAt(idx))) {
+        re += s.charAt(idx ++);
+      } else {
+        let cnt = 0;
+        while (idx < s.length && /[0-9]/.test(s.charAt(idx))) {
+          cnt = cnt * 10 + parseInt(s.charAt(idx ++), 10);
+        }
+        idx ++;
+        const cur = recursion();
+        idx ++;
+        while (cnt -- > 0) {
+          re += cur;
+        }
+      }
+    }
+    return re;
+  };
+  let idx = 0;
+  return recursion();
 };
 
 
