99c95cd39c80ada684a7b505ad3181209c5ed539
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-19 16:01:37 -0700

    task0397 - replace integer: finished

--- task0397-replace-int/replace-int.js
+++ task0397-replace-int/replace-int.js
@@ -3,7 +3,18 @@
  * @return {number}           minimum time to become 1
  */
 const integerReplacement = function(n) {
-
+  let re = 0;
+  while (n > 1) {
+    if (( n & 1) === 0) {
+      n = Math.floor(n / 2);
+    } else if (n === 3 || (Math.floor(n / 2) & 1) === 0) {
+      n --;
+    } else {
+      n ++;
+    }
+    re ++;
+  }
+  return re;
 };
 
 
@@ -16,6 +27,7 @@ const main = (callback) => {
   [
     8,
     7,
+    65535,
   ].forEach( v => {
     console.log(`  num=${v}, rep=${integerReplacement(v)}`);
   });
