a4a75360a91bb9bcc6b7189c2893fa56a6926eb8
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-19 15:14:23 -0700

    task0395 - max substring with k+ duplicated chars: finished

--- task0395-max-sub-k-dups/max-sub-k-dups.js
+++ task0395-max-sub-k-dups/max-sub-k-dups.js
@@ -4,7 +4,23 @@
  * @return {number}         the max sub string
  */
 const longestSubstring = function(s, k) {
-
+  const recursion = (s) => {
+    if (s.length < k) {
+      return 0;
+    }
+    const map = {};
+    for (let i = 0; i < s.length; i++) {
+      const char = s.charAt(i);
+      map[char] = 1 + (map[char] || 0);
+    }
+    let idx = 0;
+    while (idx < s.length && map[s.charAt(idx)] >= k) {
+      idx ++;
+    }
+    return idx < s.length ? Math.max(recursion(s.substr(0, idx)), recursion(s.substr(idx + 1)))
+                          : s.length;
+  };
+  return k === 0 ? s.length : recursion(s);
 };
 
 
@@ -17,6 +33,7 @@ const main = (callback) => {
   [
     ['aaabb', 3],
     ['ababbc', 2],
+    ['bbaaacbd', 3],
   ].forEach( vs => {
     console.log(`  max=${longestSubstring(...vs)}, k=${vs[1]}, str="${vs[0]}"`);
   });
