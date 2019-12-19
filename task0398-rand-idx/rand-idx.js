2c8ba70d30e0d40c4fcca57ba867f61dbd6618e2
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-19 16:25:14 -0700

    task0398 - random pick index: finished

--- task0398-rand-idx/rand-idx.js
+++ task0398-rand-idx/rand-idx.js
@@ -1,12 +1,45 @@
 /**
  * @param {number[]}    nums    the number array
  */
+const Solution0 = function(nums) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const k = `${nums[i]}`;
+    map[k] = map[k] || [];
+    map[k].push(i);
+  }
+
+  /** 
+   * @param {number}    target    the number to pick
+   * @return {number}             a random index of the target number
+   */
+  this.pick = (target) => {
+    const idxs = map[`${target}`];
+    return idxs[Math.floor(Math.random() * idxs.length)];
+  };
+};
+
+
+/**
+ * @param {number[]}    nums    the number array
+ */
 const Solution = function(nums) {
   /** 
    * @param {number}    target    the number to pick
    * @return {number}             a random index of the target number
    */
   this.pick = (target) => {
+    let re = -1;
+    let cnt = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== target) {
+        continue ;
+      }
+      if (Math.floor(Math.random() * (++ cnt)) === 0) {
+        re = i;
+      }
+    }
+    return re;
   };
 };
 
