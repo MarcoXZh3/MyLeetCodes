8d1d883f2e589392a029bbb1cf4dcd1f9e3fb00d
MarcoXZh3 <marcox@durabuiltwindows.com>
2019-12-20 16:39:06 -0700

    task0400 - nth digit: finished

--- task0400-nth-digit/nth-digit.js
+++ task0400-nth-digit/nth-digit.js
@@ -3,7 +3,16 @@
  * @return {number}         the nth digit
  */
 const findNthDigit = function(n) {
-
+  let len = 1;
+  let cnt = 9;
+  let start = 1;
+  while (n > len * cnt) {
+    n -= len * cnt;
+    len ++;
+    cnt *= 10;
+    start *= 10;
+  }
+  return parseInt(`${start + Math.floor((n - 1) / len)}`.charAt((n - 1) % len), 10);
 };
 
 
