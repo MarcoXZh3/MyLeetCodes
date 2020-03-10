82a118d5513024d137e4229f948e3aad1d1746b4
MarcoX <marcox@durabuiltwindows.com>
2020-03-10 15:34:36 -0600

    task0402 - remove k digits: finished

--- task0402-del-k-digits/del-k-digits.js
+++ task0402-del-k-digits/del-k-digits.js
@@ -4,7 +4,22 @@
  * @return {string}             the minimum number after removing
  */
 const removeKdigits = function(num, k) {
-
+  num = num.split('').map( v => parseInt(v, 10) );
+  const len = num.length - k;
+  const stack = Array(num.length).fill(0);
+  let idx = 0;
+  for (let i = 0; i < num.length; i++) {
+    while (idx > 0 && stack[idx - 1] > num[i] && k > 0) {
+      idx --;
+      k --;
+    }
+    stack[idx ++] = num[i];
+  }
+  idx = 0;
+  while (idx < len && stack[idx] === 0) {
+    idx ++;
+  }
+  return idx === len ? '0' : stack.slice(idx, len).join('');
 };
 
 
