b94b94042795a6290718a9f2abafb2635fbb37f6
MarcoX <marcox@durabuiltwindows.com>
2020-03-12 14:52:06 -0600

    task0409 - longest palindrome: finished

--- task0409-max-palindrome/max-palindrome.js
+++ task0409-max-palindrome/max-palindrome.js
@@ -3,7 +3,18 @@
  * @return {number}         length of the max palindrome
  */
 const longestPalindrome = function(s) {
-
+  let res = 0;
+  const set = new Set();
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    if (set.has(char)) {
+      res ++;
+      set.delete(char);
+    } else {
+      set.add(char);
+    }
+  }
+  return res * 2 + (set.size > 0 ? 1 : 0);
 };
 
 
