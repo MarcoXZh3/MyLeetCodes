f6dbc1a2f4c4d4d407670e2292357d5ad8155fa4
MarcoX <marcox@durabuiltwindows.com>
2020-03-13 16:10:44 -0600

    task0415 - add strings: finished

--- task0415-add-strings/add-strings.js
+++ task0415-add-strings/add-strings.js
@@ -4,7 +4,17 @@
  * @return {string}           the sum of the two numbers
  */
 const addStrings = function(num1, num2) {
-
+  num1 = num1.split('');
+  num2 = num2.split('');
+  let re = '';
+  let carry = 0;
+  while (num1.length > 0 || num2.length > 0 || carry > 0) {
+    const n1 = num1.length > 0 ? parseInt(num1.pop(), 10) : 0;
+    const n2 = num2.length > 0 ? parseInt(num2.pop(), 10) : 0;
+    re = `${(n1 + n2 + carry) % 10}${re}`;
+    carry = Math.floor((n1 + n2 + carry) / 10);
+  }
+  return re;
 };
 
 
