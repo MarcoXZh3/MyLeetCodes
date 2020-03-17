0b16c9b4913c708130360288b6218d3a37e63f33
MarcoX <marcox@durabuiltwindows.com>
2020-03-17 09:28:22 -0600

    task0416 - split equal sub set: finished

--- task0416-equal-sub-set/equal-sub-set.js
+++ task0416-equal-sub-set/equal-sub-set.js
@@ -3,7 +3,24 @@
  * @return {boolean}            whether can be split equally or not
  */
 const canPartition = function(nums) {
-
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  if (sum % 2 !== 0) {
+    return false;
+  }
+  sum /= 2;
+  const dp = [...Array(sum + 1).keys()].map( _=>false );
+  dp[0] = true;
+  for (let num of nums) {
+    for (let i = sum; i > 0; i--) {
+      if (i >= num) {
+        dp[i] = dp[i] || dp[i - num];
+      }
+    }
+  }
+  return dp[sum];
 };
 
 
@@ -16,6 +33,7 @@ const main = (callback) => {
   [
     [1, 5, 11, 5],
     [1, 2, 3, 5],
+    [1, 2, 5],
   ].forEach( vs => {
     console.log(`  canPartition=${canPartition(vs)}, arr=${JSON.stringify(vs)}`);
   });
