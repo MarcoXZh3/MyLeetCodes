ed65c9540ba7fb1eb34ab529158a4988ed901f40
MarcoX <marcox@durabuiltwindows.com>
2020-03-17 12:11:23 -0600

    task0419 - battleships in board: finished

--- task0419-ship-board/ship-board.js
+++ task0419-ship-board/ship-board.js
@@ -3,7 +3,23 @@
  * @return {number}                 how many ships in the board
  */
 const countBattleships = function(board) {
-
+  const m = board.length;
+  if (m === 0) {
+    return 0;
+  }
+  const n = board[0].length;
+  let re = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === '.' ||
+          i > 0 && board[i - 1][j] === 'X' ||
+          j > 0 && board[i][j - 1] === 'X') {
+        continue ;
+      }
+      re ++;
+    }
+  }
+  return re;
 };
 
 
