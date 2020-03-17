83c36df4c361a2bbe2559cfad83e52324691ef0b
MarcoX <marcox@durabuiltwindows.com>
2020-03-17 13:17:39 -0600

    task0420 - strong password checker: finished

--- task0420-strong-pass-check/strong-pass-check.js
+++ task0420-strong-pass-check/strong-pass-check.js
@@ -3,7 +3,55 @@
  * @return {number}         how many changes needed to become complicated
  */
 const strongPasswordChecker = function(s) {
-
+  let res = 0;
+  let lower = 1;
+  let upper = 1;
+  let num = 1;
+  let arr = Array(s.length).fill(0);
+  for (let i = 0; i < s.length;) {
+    const char = s.charAt(i);
+    if (/[a-z]/.test(char)) {
+      lower = 0;
+    } else if (/[A-Z]/.test(char)) {
+      upper = 0;
+    } else if (/[0-9]/.test(char)) {
+      num = 0;
+    }
+    let j = i;
+    while (i < s.length && char === s.charAt(i)) {
+      i ++;
+    }
+    arr[j] = i - j;
+  }
+  let total = lower + upper + num;
+  if (s.length < 6) {
+    res += total + Math.max(0, 6 - s.length - total);
+  } else {
+    let over = Math.max(s.length - 20, 0);
+    let left = 0;
+    res += over;
+    for (let k = 1; k < 3; k++) {
+      for (let i = 0; i < s.length && over > 0; i++) {
+        if (arr[i] < 3 || arr[i] % 3 !== k - 1) {
+          continue ;
+        }
+        arr[i] -= Math.min(over, k);
+        over -= k;
+      }
+    }
+    for (let i = 0; i < s.length; i++) {
+      if (arr[i] >= 3 && over > 0) {
+        let need = arr[i] - 2;
+        arr[i] -= over;
+        over -= need;
+      }
+      if (arr[i] >= 3) {
+        left += Math.floor(arr[i] / 3);
+      }
+    }
+    res += Math.max(total, left);
+  }
+  return res;
 };
 
 
